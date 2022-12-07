f288f806d92d0aadb960704148282fce4837a1ca
kosuke-codeman3 <siratamatabetai@gmail.com>
2022-12-07 16:18:08 +0900

    feat: hamburger menuクリック時にメニュー表示する機能追加

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,18 @@ import $ from "jquery";
 
 $(function() {
   console.log('環境構築完了');
+
+  $('.l-header__hamburger').on('click', menuClick);
+  
+  function menuClick() {
+    let className = document.getElementById("header__menu").className;
+    if(className == "l-header__menu") {
+      document.getElementById("header__menu").className = "l-header__menu-display";
+    }
+    else {
+      document.getElementById("header__menu").className = "l-header__menu";
+    }
+  }
+
 })
+
