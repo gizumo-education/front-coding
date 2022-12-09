3aa595efe1ea52970baa4772aea5414ec229c065
kosuke-codeman3 <siratamatabetai@gmail.com>
2022-12-09 09:51:47 +0900

    feat:htmlの各sectionにid設定

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,20 @@ $(function() {
   console.log('環境構築完了');
 
   $('.l-header__hamburger').on('click', menuClick);
+
+  $('a[href^="#"]').on('click', function(){
+    //スクロールのスピード
+    let speed = 500;
+    //リンク元を取得
+    let href= $(this).attr("href");
+    //リンク先を取得
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    //リンク先までの距離を取得
+    let position = target.offset().top;
+    //スムーススクロール
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
   
   function menuClick() {
     console.log('click');
