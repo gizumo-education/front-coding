a99842d01c9d73f02b06ced1922af409621cde1e
Your Name <sssz6915@yahoo.co.jp>
2023-02-19 12:52:51 +0900

    add:メニューリンクの実装

--- src/js/app.js
+++ src/js/app.js
@@ -3,25 +3,43 @@ import "slick-carousel";
 
 $('.slider').slick({
   slider: $('.p-news__container-box'),
-  autoplay: false,//自動的に動き出すか。初期値はfalse。
-  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-  slidesToShow: 4,//スライドを画面に●枚見せる
-  slidesToScroll: 1,//1回のスクロールで●枚の写真を移動して見せる
+  autoplay: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
   arrows: true,
-  prevArrow: $('.p-news__container__left'),//矢印部分PreviewのHTMLを変更
-  nextArrow: $('.p-news__container__right'),//矢印部分NextのHTMLを変更
-  dots: false,//下部ドットナビゲーションの表示
+  prevArrow: $('.p-news__container__left'),
+  nextArrow: $('.p-news__container__right'),
+  dots: false,
   responsive: [
     {
-      breakpoint: 767,//モニターの横幅が●px以下の見せ方
+      breakpoint: 767,
       settings: {
-        slidesToShow: 1,//スライドを画面に●枚見せる
-        slidesToScroll: 1,//1回のスクロールで●枚の写真を移動して見せる
+        slidesToShow: 1,
+        slidesToScroll: 1,
       }
     }
   ]
 });
 
-$(".openbtn").click(function () {
+//ボタンをクリック
+$('.openbtn').on('click', function () {
   $(this).toggleClass('active');
+  $('.p-menu').toggleClass('panelactive');
+  $('body').toggleClass('no_scroll');
+});
+//リンクをクリック
+$(function(){
+  $('a[href^="#"]').on('click' ,function(){
+    $('.p-menu').removeClass('panelactive');
+    $(".openbtn").removeClass('active');
+    $('body').removeClass('no_scroll');
+    var adjust = -50;
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top + adjust;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
 });
