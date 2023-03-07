ffb61abd3e7cbdbaa77b6f24170b6a444379a45c
Onodera Yuki <ondryuki@gmail.com>
2023-03-07 14:20:43 +0900

    feat: スムーズスクロールの機能を追加

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,15 @@
 import $ from "jquery";
 
-$(function() {
+$(function () {
   console.log('環境構築完了');
-})
+
+  $(function () {
+    $('a[href^="#"]').on('click', function () {
+      var href = $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top - 150;
+      $('body,html').animate({ scrollTop: position }, 400, 'swing');
+      return false;
+    });
+  });
+});
