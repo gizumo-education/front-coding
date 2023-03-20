5351153c8afe2732b56c3755a87918e1ff34c78f
kanai-sarina <k.s3192@icloud.com>
2023-03-20 18:34:12 +0900

    add:ハンバーガーメニュー作成スムーススクロール作成

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,23 @@ import $ from "jquery";
 $(function() {
   console.log('環境構築完了');
 })
+
+const ham = $('#js-header__nav__toggle');
+const nav = $('#js-header__nav');
+ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+  ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+  nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+});
+
+const header = $('#header');
+
+$('a[href^="#"]').on('click', function() {
+  const gap = header.outerHeight();
+  const speed = 500;
+  const href = $(this).attr("href");
+  const target = $(href == "#" || href == "" ? "html" : href);
+  const position = target.offset().top - gap;
+  
+  $("html, body").animate({ scrollTop: position }, speed, "swing");
+  return false;
+});
