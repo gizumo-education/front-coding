ae0b4820f1b4b83e3a8c054ded630ced77ff5369
Joex13 <aokijo0113p@gmail.com>
2023-03-20 15:51:27 +0900

    feat: ハンバーガーメニューの表示トグルまで実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,15 @@
-import $ from "jquery";
+import $ from 'jquery';
 
-$(function() {
-  console.log('環境構築完了');
-})
+$(function () {
+  // console.log('環境構築完了');
+  $('.c-hamburger').on('click', function () {
+    $(this).toggleClass('js-active');
+    $('.l-header__nav__list--sp').toggleClass('js-active');
+    // スクロールロックのトグル処理
+    if ($('html').css('overflow-y') === 'visible') {
+      $('html').css('overflow-y', 'hidden');
+    } else {
+      $('html').css('overflow-y', 'visible');
+    }
+  });
+});
