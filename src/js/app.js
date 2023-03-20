d88a841f75fafb4aebe2e39ef798f3b6d7ed6bf3
Joex13 <aokijo0113p@gmail.com>
2023-03-20 16:22:03 +0900

    fix: スクロールロックバグを仮修正（画面サイズ増加時）

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,14 @@ $(function () {
   $('.c-hamburger').on('click', function () {
     $(this).toggleClass('js-active');
     $('.l-header__nav__list--sp').toggleClass('js-active');
+    
     // スクロールロックのトグル処理
-    if ($('html').css('overflow-y') === 'visible') {
-      $('html').css('overflow-y', 'hidden');
+    if ($('body').css('overflow-y') === 'visible') {
+      $('body').css('overflow-y', 'hidden');
     } else {
-      $('html').css('overflow-y', 'visible');
+      $('body').css('overflow-y', 'visible');
     }
+
+    // 
   });
 });
