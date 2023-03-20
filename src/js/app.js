af57b1435c08bbc5ff76d974f371d0c88c3b4505
Joex13 <aokijo0113p@gmail.com>
2023-03-20 17:39:02 +0900

    feat: Swiperの初期導入完了（動きます）

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
 import $ from 'jquery';
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 $(function () {
-  // console.log('環境構築完了');
+  console.log('環境構築完了');
   $('.c-hamburger').on('click', function () {
     $(this).toggleClass('js-active');
     $('.l-header__nav__list--sp').toggleClass('js-active');
-    
+
     // スクロールロックのトグル処理
     if ($('body').css('overflow-y') === 'visible') {
       $('body').css('overflow-y', 'hidden');
@@ -13,6 +16,26 @@ $(function () {
       $('body').css('overflow-y', 'visible');
     }
 
-    // 
+    //
+  });
+});
+
+// スライダー実装ここから
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('vanilla js called');
+  Swiper.use(Navigation);
+  new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      768: {
+        slidesPerView: 4,
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
 });
