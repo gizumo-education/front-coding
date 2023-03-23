2e6eab5725a17d6bbadcbbf9579ef74fc2d3c548
吉村優芽 <yumeyoshimura@yoshimurayumenoMacBook-Air.local>
2023-03-23 10:36:35 +0900

    add: slidesToScrollの値に関係なく、スワイプ量に応じてスライドさせる処理の追加

--- src/js/app.js
+++ src/js/app.js
@@ -43,6 +43,8 @@ $(function() {
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
+    swipe: true,
+    swipeToSlide: true,
     prevArrow: '<button class="slide-arrow prev-arrow"></button>',
     nextArrow: '<button class="slide-arrow next-arrow"></button>',
     responsive: [{
