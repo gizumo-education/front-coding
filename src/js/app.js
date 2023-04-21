89d2c035f2d4d5aaf740cd61c3404f4429ce8f3c
ta9to <ta9to@TakutonoMacBook-Pro.local>
2023-04-21 18:24:25 +0900

    feat:ハンバーガーボタン及びページ内リンク機能追加

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,34 @@ import $ from "jquery";
 
 $(function() {
   console.log('環境構築完了');
+
+  $('.l-header__hamburger').on('click', activeBtn);
+  let state = false;
+    function activeBtn() {
+      $('.l-header__open-line').toggleClass('is-active');
+      $('.l-header__slidedown').stop().toggleClass('toggle-scroll');
+      let scroll = $(window).scrollTop();
+      if (state == false) {
+        $('body').addClass('is-fixed').css({'top': -scroll});
+        state = true;
+      } else {
+        $('body').removeClass('is-fixed').css({'top': 0});
+        state = false;
+      }
+  };
+
+  $('a[href^="#"]').on('click', hideMenu);
+  let headerHeight = $('.l-header').outerHeight();
+  let speed = 0;
+    function hideMenu() {
+      $('.l-header__open-line').removeClass('is-active');
+      $('.l-header__slidedown').removeClass('toggle-scroll');
+      $('body').removeClass('is-fixed').css({'top': 0});
+      let href= $(this).attr("href");
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      let position = target.offset().top - headerHeight;
+      $('html, body').stop().animate({scrollTop:position}, speed, "swing");
+      state = false;
+      return false;
+  }
 })
