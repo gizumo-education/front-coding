45cafb8ceef54b5c2d65442c65830420a6af3334
Your Name <you@example.com>
2023-04-26 14:28:37 +0900

    fix:slickの矢印調整

--- src/js/app.js
+++ src/js/app.js
@@ -33,5 +33,15 @@ $(function() {
     autoplay: true,
     autoplaySpeed: 3000,
     infinite: true,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }],
   });
 })
