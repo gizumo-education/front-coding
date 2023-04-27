7a6b1f87f16256f58cdaae96678bd985e0fa6330
Nabezo199 <rznabekura1203@gmail.com>
2023-04-27 11:40:25 +0900

    fix & add:　articleの要素追加しslide機能正常化、メニュークリック時のスクロールダウン追加

--- src/js/app.js
+++ src/js/app.js
@@ -12,11 +12,16 @@ $('.l-header__hamburgerMenu, .l-header__hamburgerMenu__closeIcon').click(functio
   scrollToggle();
 });
 
-$('.l-header__hamburgerMenu__inside__box__text').click(function() {
+$('.l-header__hamburgerMenu__inside__box__text').click(function(e) {
   $('.l-header__hamburgerMenu__inside').toggleClass("u-active__menu");
   $(".l-header__hamburgerMenu__closeIcon").toggleClass("u-active__icon");
   $('.l-header__hamburgerMenu').removeClass("u-hidden");
   scrollToggle();
+  e.preventDefault();
+  var headerHight = $("#header").height();
+  var target = $(this).attr("href");
+  var position = $(target).offset().top - headerHight;
+  $("html, body").animate({scrollTop: position}, 1500);
 });
 
 function scrollToggle() {
@@ -35,7 +40,7 @@ $('.p-article__list').slick({
   slidesToScroll: 1,
   swipe: true,
   swipeToSlide: true,
-  arrow: true,
+  arrows: true,
   prevArrow: '<div class="p-article__list__arrow p-article__list__prev"></div>',
   nextArrow: '<div class="p-article__list__arrow p-article__list__next"></div>',
   responsive: [{
