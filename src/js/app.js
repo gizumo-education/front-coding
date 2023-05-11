fcbcfd0359dcbf31bd99ea6fb93fa7ac3d5f138b
堀家將平 <shohei@horikenoMacBook-Pro.local>
2023-05-11 12:44:04 +0900

    SPハンバーガーメニュー実装

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,10 @@ import $ from "jquery";
 $(function() {
   console.log('環境構築完了');
 })
+
+$('#js-hamburger').on('click', () => {
+  console.log('clicked!')
+  $('#js-hamburger').toggleClass("active");
+  $('#js-hamburger__items').toggleClass("active");
+  $('body').toggleClass("active");
+});
