92694b41e08ce2ab707619bd1267de8cb68c27fe
Christopher-90 <kilika.10.pher@gmail.com>
2023-05-31 15:42:32 +0900

    feat: ハンバーガーメニュー展開時にスクロールを禁止する

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,10 @@ import $ from "jquery";
 $(function() {
   console.log('環境構築完了');
 
-  var authenticity = false;
-
   $('.c-hamburger').click(function() {
     $('.c-hamburger__patty').toggleClass('active');
     $('.p-contentsLink').toggleClass('active');
-
+    $('body').toggleClass('u-no-scroll');
   });
 });
+
