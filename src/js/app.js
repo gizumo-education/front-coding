b055399ae02a51755cd04340dd93d3166a11e424
Christopher-90 <kilika.10.pher@gmail.com>
2023-05-31 15:11:25 +0900

    feat: ハンバーガーメニューをクリック時右から左にスライドインとaタグのlinkの設定

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,11 @@ import $ from "jquery";
 $(function() {
   console.log('環境構築完了');
 
+  var authenticity = false;
+
   $('.c-hamburger').click(function() {
     $('.c-hamburger__patty').toggleClass('active');
+    $('.p-contentsLink').toggleClass('active');
+
   });
-})
+});
