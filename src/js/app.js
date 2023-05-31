0437a67f29bd439f0d7da32433a0d7398f453308
Christopher-90 <kilika.10.pher@gmail.com>
2023-05-31 15:55:41 +0900

    feat: aタグクリック時に画面をスクロールさせる

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,22 @@ import $ from "jquery";
 $(function() {
   console.log('環境構築完了');
 
+  // ハンバーガーメニュー
   $('.c-hamburger').click(function() {
     $('.c-hamburger__patty').toggleClass('active');
     $('.p-contentsLink').toggleClass('active');
     $('body').toggleClass('u-no-scroll');
+  });
+
+  // スムーズなスクロール
+  $('a[href^="#"]').click(function(event) {
+    event.preventDefault();
+    var target = $(this.hash);
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 400);
+    }
   });
 });
 
