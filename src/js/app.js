82e11a7a9e1dfce3326dfe6ad33fbd2e411b9b30
MACBOOK <3aos4682@gmail.com>
2023-06-12 11:15:20 +0900

    js機能実装

---
+++ src/js/app.js
@@ -0,0 +1,43 @@
+import $ from "jquery";
+import 'slick-carousel';
+
+$(function() {
+  console.log('環境構築完了');
+})
+
+//ハンバーガー
+$(".c-burger-btn").on('click',function(){
+  $('.p-drawer').toggleClass('open');
+  $('body').toggleClass('noscroll');
+});
+
+$("a[href^='#']:not([href='#'])").click(function () {
+
+  $('body').toggleClass('noscroll');
+  $('.p-drawer').toggleClass('open');
+
+  var target = $($(this).attr("href")).offset().top;
+
+  target -=80;
+
+  $("html, body").animate({scrollTop: target}, 500);
+
+  return false;
+});
+
+$('.p-work__list').slick({
+  variableWidth: true,
+  infinite: true,
+  arrows: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
