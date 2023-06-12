1d5bbf93382c1b5982f8c9b58abff53c8541cd69
斎藤雅広 <saitoumasahiro@saitoumasahironoMacBook-Air.local>
2023-06-12 23:07:49 +0900

    fix: ハンバーガーメニュー上部の崩れ,Xボタンのページ表示の修正,constでの再定義

--- src/js/app.js
+++ src/js/app.js
@@ -7,43 +7,37 @@ $(function() {
 
 
 // ハンバーガーをクリック後Xに
-$(function () {
-  $('.c-button__hamburger__bar').on('click', function () {
-    $('.c-button__hamburger__bar__span').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+$(function() {
+  $('.c-button__hamburger__bar').on('click', function() {
+    $('.c-button__hamburger__bar__span').toggleClass('open'); 
   })
 });
 
 
 // 背景固定
-var state = false;
-var scrollpos;
-$('.c-button__hamburger__bar').on('click', function(){
-  $('ul').toggleClass('open');
-  if(state == false) {
-    scrollpos = $(window).scrollTop();
-    $('body').addClass('fixed').css({'top': -scrollpos});
-    state = true;
-  } else {
-    $('body').removeClass('fixed').css({'top': 0});
-    state = false;
-  }
+$(function () {
+  $(".c-button__hamburger__bar").click(function() {
+    $(this).toggleClass("active");
+    $(".c-button__hamburger__content").toggleClass("active");
+    $("body").toggleClass("active");
+  });
 });
 
 
 // リンククリック時ハンバーガーメニュー閉じて移動
-$('.c-button__hamburger__content__list__title').on('click', function(event) {
+$('.c-button__hamburger__content__list__title').on('click', function() {
   $('.c-button__hamburger__bar').trigger('click');
 });
 
 // ページ内へスクロール
-$('a[href^="#"]').click(function () {
-  var adjust = 100;
-  var speed = 500;
-  var href = $(this).attr("href");
-  var target = $(href == "#" || href == "" ? 'html' : href);
-  var position = target.offset().top - adjust;
+$('a[href^="#"]').click(function() {
+  const adjust = 60;
+  const speed = 500;
+  const href = $(this).attr("href");
+  const target = $(href == "#" || href == "" ? 'html' : href);
+  const position = target.offset().top - adjust;
   $('body,html').animate({ scrollTop: position }, speed, 'swing');
-  return false;
+  return;
 });
 
 
