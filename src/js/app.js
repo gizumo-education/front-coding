60449fa8c90a12377f1eb17de92a849492410ce8
y-hashimoto-2023 <yukhashimo01@gmail.com>
2023-07-04 12:27:25 +0900

    refactor: 同値演算子に修正

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ $('a[href^="#"]').click(function () {
   event.preventDefault();
   const speed = 400;
   const href = $(this).attr("href");
-  const target = $(href == "#" || href == "" ? "html" : href);
+  const target = $(href === "#" || href === "" ? "html" : href);
   const position = target.offset().top;
   $("body,html").animate({ scrollTop: position - 65 }, speed, "swing");
 });
