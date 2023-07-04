1962f2f86bd4c95f1c8fadae9022b9f5acfecae7
y-hashimoto-2023 <yukhashimo01@gmail.com>
2023-07-04 12:31:32 +0900

    refactor: jQuery処理記述修正

--- src/js/app.js
+++ src/js/app.js
@@ -3,32 +3,32 @@ import 'slick-carousel';
 
 $(function() {
   console.log('環境構築完了');
+
+  $('a[href^="#"]').click(function () {
+    event.preventDefault();
+    const speed = 400;
+    const href = $(this).attr("href");
+    const target = $(href === "#" || href === "" ? "html" : href);
+    const position = target.offset().top;
+    $("body,html").animate({ scrollTop: position - 65 }, speed, "swing");
+  });
+  
+  $('.l-header__hamburger').on('click', function(){
+    $(this).toggleClass('is-active');
+    $('.l-header__sp-menu').toggleClass('show-menu');
+    $('body').toggleClass('scroll-prevent');
+  });
+  
+  $('.l-header__sp-menu ul li a').click(function() {
+    $('.l-header__hamburger').trigger('click');
+  });
+  
+  $('.p-recruit__inner__list').slick({
+    adaptiveHeight: true,
+    variableWidth: true,
+    prevArrow: '<button type=button class=slick-prev></button>',
+    nextArrow: '<button type=button class=slick-next></button>',
+    speed: 300,
+    slidesToShow: 1,
+  });
 })
-
-$('a[href^="#"]').click(function () {
-  event.preventDefault();
-  const speed = 400;
-  const href = $(this).attr("href");
-  const target = $(href === "#" || href === "" ? "html" : href);
-  const position = target.offset().top;
-  $("body,html").animate({ scrollTop: position - 65 }, speed, "swing");
-});
-
-$('.l-header__hamburger').on('click', function(){
-  $(this).toggleClass('is-active');
-  $('.l-header__sp-menu').toggleClass('show-menu');
-  $('body').toggleClass('scroll-prevent');
-});
-
-$('.l-header__sp-menu ul li a').click(function() {
-  $('.l-header__hamburger').trigger('click');
-});
-
-$('.p-recruit__inner__list').slick({
-  adaptiveHeight: true,
-  variableWidth: true,
-  prevArrow: '<button type=button class=slick-prev></button>',
-  nextArrow: '<button type=button class=slick-next></button>',
-  speed: 300,
-  slidesToShow: 1,
-});
