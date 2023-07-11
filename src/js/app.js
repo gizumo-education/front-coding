c6b6b44f8d8e5b005b1df26279e2dbe7582d94c5
iriguthi <iriguthi.ryoma@gmail.com>
2023-07-11 10:38:13 +0900

    js機能実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,49 @@
 import $ from "jquery";
+import "slick-carousel";
 
 $(function() {
-  console.log('環境構築完了');
+  //console.log('環境構築完了');
+  
+  $('.l-header__toggle').on('click', function(){
+    $(this).toggleClass('is-active');
+    $('.nav_content').toggleClass('is-open');
+    $("body").toggleClass('no-scroll')
+  });
+
+  $('a[href*="#"]').on('click', function(){
+    const href = $(this).attr("href");
+    const target = $(href === "#" || href === "" ? "html" : href);
+    const position = target.offset().top;
+    $("body,html").animate({ scrollTop: position - 65});
+    return;
+  });
+
+let slidesToShowNum = 4;
+let item = $('.p-news-images__image').length;
+//console.log(item);
+if (item <= slidesToShowNum) {
+  for (let i = 0; i <= slidesToShowNum + 1 - item ; i++) {
+    $('.p-news-images__image:nth-child('+ i + ')').clone().appendTo('.p-news-images__group');
+  }
+}
+  $('.p-news-images__group').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 6000,
+    cssEase: "linear",
+    slidesToShow: slidesToShowNum,
+    swipe: false,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover:false,
+
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+    ]
+  });
 })
