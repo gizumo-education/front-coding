1d420aa3c46cc2f3d578c2d94ca0c3f9d3041ec3
中村優穂 <yuho@nakamurayuuhonoMacBook-Pro-257.local>
2023-07-20 16:30:15 +0900

    feat:js機能の実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,21 @@
 import $ from "jquery";
 
-$(function() {
+$(function () {
   console.log('環境構築完了');
+
+
+  //hamburger
+  const ham = $('#js-hamburger');
+  const nav = $('#js-nav');
+  ham.on('click', function () {
+    ham.toggleClass('active');
+    nav.toggleClass('active');
+    $("body").toggleClass("active");
+
+  });
+
+  //slick　カルーセル
+
+
 })
+
