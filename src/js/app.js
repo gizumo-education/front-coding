f3da3556c2cc5013959f6bc7321e34ab68050f05
中村優穂 <yuho@nakamurayuuhonoMacBook-Pro-257.local>
2023-07-21 16:04:57 +0900

    feat:スムーススクロールの実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 import $ from "jquery";
+import 'slick-carousel';
+
 
 $(function () {
-  console.log('環境構築完了');
-
-
-  //hamburger
+  console.log('環境構築完了')
+  //ハンバーガーメニューの挙動
   const ham = $('#js-hamburger');
   const nav = $('#js-nav');
   ham.on('click', function () {
@@ -14,6 +14,34 @@ $(function () {
   });
 
   //slick　カルーセル
+  $('.slick').slick({
+    slideshowSpeed: 4000,
+    fade: true,
+    slidesToShow: 1,
+    prevArrow: $('.custom-prev-arrow'),
+    nextArrow: $('.custom-next-arrow'),
+  });
+
+
+
+
+  //スムーススクロール
+  const header = $('#header');
+  $('a[href^="#"]').on('click', function () {
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    const gap = header.outerHeight();
+    let position = target.offset().top - gap;
+    var speed = 500;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    //ハンバーガーメニュー動作時の挙動
+    nav.toggleClass('active');
+    ham.toggleClass('active');
+    $("body").toggleClass("active")
+  })
+
+
+
 
 
 })
