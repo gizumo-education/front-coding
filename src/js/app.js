e16ab0914da676e384ed19e2abd032c9be9ba5b1
中村優穂 <yuho@nakamurayuuhonoMacBook-Pro-257.local>
2023-07-23 11:19:19 +0900

    update:ハンバーガーメニューの実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,15 @@
 import $ from "jquery";
 
-$(function() {
+$(function () {
   console.log('環境構築完了');
+
+  //ハンバーガーメニュー
+  const ham = $('#js-hamburger');
+  const nav = $('#js-nav');
+  ham.on('click', function () {
+    ham.toggleClass('active');
+    nav.toggleClass('active');
+
+  });
+
 })
