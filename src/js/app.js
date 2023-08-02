e1c0ccbf744848999492b1de70ac9995bf47b5e1
Tomoyuki Tanak <tomoyukitanaka@TomoyukinoMacBook-Pro.local>
2023-08-02 16:11:03 +0900

    新着求人のUI,カルーセル実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,18 @@
 import $ from "jquery";
+import 'slick-carousel';
 
 $(function() {
   console.log('環境構築完了');
 })
+
+$(function () {
+  $(".newrequest__wrapper").slick({
+    autoplay: true, //自動再生
+    infinite: false, //スライドのループ有効化
+    dots: true, //ドットのナビゲーションを表示
+    slidesToShow: 4, //表示するスライドの数
+    slidesToScroll: 1, //スクロールで切り替わるスライドの数
+    prevArrow: '<button class="slide-arrow slick-prev"></button>',
+    nextArrow: '<button class="slide-arrow slick-next"></button>',
+  });
+});
