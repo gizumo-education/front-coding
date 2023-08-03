3d46595603c4da83c6ea9273f3252e1950bc2195
Tomoyuki Tanak <tomoyukitanaka@TomoyukinoMacBook-Pro.local>
2023-08-03 10:12:44 +0900

    fix:aタグのスタイル追加

--- src/js/app.js
+++ src/js/app.js
@@ -9,10 +9,24 @@ $(function () {
   $(".p-newrequest__wrapper").slick({
     autoplay: true, //自動再生
     infinite: false, //スライドのループ有効化
-    dots: true, //ドットのナビゲーションを表示
+    dots: true, //ドットのナビゲーションを表示 削除予定
     slidesToShow: 4, //表示するスライドの数
     slidesToScroll: 1, //スクロールで切り替わるスライドの数
     prevArrow: '<button class="slide-arrow slick-prev"></button>',
     nextArrow: '<button class="slide-arrow slick-next"></button>',
+        responsive: [{
+      breakpoint: 1199, //ブレークポイントが768px
+      settings: {
+        slidesToShow: 1, //表示するスライドの数
+        slidesToScroll: 1, //スクロールで切り替わるスライドの数
+      }
+    }]
+    // }, {
+    //   breakpoint: 480, //ブレークポイントが480px
+    //   settings: {
+    //     slidesToShow: 2, //表示するスライドの数
+    //     slidesToScroll: 2, //スクロールで切り替わるスライドの数
+    //   }
+    // }]
   });
 });
