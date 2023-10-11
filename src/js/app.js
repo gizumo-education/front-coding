df8298533f45e6e133947027e590a17bc4fe88f1
giz-hiro <hiro.design03@gmail.com>
2023-10-11 18:55:20 +0900

    JS機能実装(修正版)

--- src/js/app.js
+++ src/js/app.js
@@ -40,3 +40,13 @@ $('.p-recruit__lists').slick({
     }
   ]
 });
+
+// ページ内スクロール
+$('a[href^="#"]').click(function () {
+  const speed = 600;
+  let href = $(this).attr("href");
+  let target = $(href == "#" || href == "" ? "html" : href);
+  let position = target.offset().top;
+  $("body,html").animate({ scrollTop: position - 80 }, speed, "swing");
+  return false;
+});
