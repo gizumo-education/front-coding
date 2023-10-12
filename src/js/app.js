559080d5138e3f92107ec9d94a68e4c5cf3319ff
giz-hiro <hiro.design03@gmail.com>
2023-10-12 11:26:33 +0900

    JS機能実装(レスポンシブコード修正)

--- src/js/app.js
+++ src/js/app.js
@@ -3,50 +3,50 @@ import 'slick-carousel';
 
 $(function() {
   console.log('環境構築完了');
-})
+  //ハンバーガーメニューの処理
+  const hamMenu = document.querySelector(".l-header__hamburger");
+  const navMenu = document.querySelector(".l-header__nav-menu");
 
-//ハンバーガーメニューの処理
-const hamMenu = document.querySelector(".l-header__hamburger");
-const navMenu = document.querySelector(".l-header__nav-menu");
-
-hamMenu.addEventListener("click", (event) => {
-  hamMenu.classList.toggle('l-header__hamburger__active');
-  navMenu.classList.toggle('l-header__nav-menu__active');
-  if (document.querySelector('.l-header__nav-menu__active')) {
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.overflow = 'auto';
-  }
-  console.log(hamMenu)
-  console.log(navMenu)
-})
-
-//スライダー表示処理
-$('.p-recruit__lists').slick({
-  autoplay: false,//自動的な動き
-  infinite: true,//スライドをループ
-  slidesToShow: 4,//スライドを画面
-  slidesToScroll: 1,//1回のスクロール
-  variableWidth:false,
-  prevArrow: '<button type="button" class="slick-arrow slick-prev">Previous</button>',//矢印部分Preview
-  nextArrow: '<button type="button" class="slick-arrow slick-next">Next</button>',//矢印部分Next
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
+  hamMenu.addEventListener("click", (event) => {
+    hamMenu.classList.toggle('l-header__hamburger__active');
+    navMenu.classList.toggle('l-header__nav-menu__active');
+    if (document.querySelector('.l-header__nav-menu__active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
     }
-  ]
-});
+    console.log(hamMenu)
+    console.log(navMenu)
+  })
 
-// ページ内スクロール
-$('a[href^="#"]').click(function () {
-  const speed = 600;
-  let href = $(this).attr("href");
-  let target = $(href == "#" || href == "" ? "html" : href);
-  let position = target.offset().top;
-  $("body,html").animate({ scrollTop: position - 80 }, speed, "swing");
-  return false;
-});
+  //スライダー表示処理
+  $('.p-recruit__lists').slick({
+    autoplay: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-arrow slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-arrow slick-next">Next</button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+  // ページ内スクロール
+  $('a[href^="#"]').click(function () {
+    const speed = 600;
+    let href = $(this).attr("href");
+    let target = $(href === "#" || href === "" ? "html" : href);
+    let position = target.offset().top;
+    $("body,html").animate({ scrollTop: position - 80 }, speed, "swing");
+    hamMenu.classList.remove('l-header__hamburger__active');
+    navMenu.classList.remove('l-header__nav-menu__active');
+    document.body.style.overflow = 'auto';
+    return false;
+  });
+})
