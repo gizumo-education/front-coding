f21cc9f2533ebd4e9d6905871fdf4318dde5e52a
taichi-watabe <124047993+taichi-watabe@users.noreply.github.com>
2023-11-17 18:12:36 +0900

    基本コーディングのjs機能実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,30 @@
 import $ from "jquery";
+import 'slick-carousel';
 
 $(function() {
   console.log('環境構築完了');
+  $('#js-hamburger').click(function() {
+    $(this).toggleClass('active')
+    $('#js-nav').toggleClass('active')
+    $('#js-noScroll').toggleClass('active')
+    $('#js-removeMenu').click(function() {
+      $('#js-hamburger').removeClass('active')
+      $('#js-nav').removeClass('active')
+      $('#js-noScroll').removeClass('active')
+    })
+  })
+  $('#js-slider').slick({
+    autoplay: true,
+    speed: 700,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  })
 })
+
