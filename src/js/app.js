8d3db94a936529cd24e58645b9777cf44fd44f98
sakigig <o0daifuku0o@gmail.com>
2023-12-26 15:09:27 +0900

    jS実装（カルーセル機能実装）

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,45 @@
 import $ from "jquery";
 import 'slick-carousel';
 
-$(function() {
-console.log('環境構築完了');
+
+$(function () {
+  console.log('環境構築完了');
 })
 
 
 
-// スムーススクロール
-// $('.p-carousel__main').slick({
-//   dots: true,
-//  });
+// カルーセル
+$('#c-slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+  ]
+});
+
 
 
 // ハンバーガーメニュー
-$(function(){
-  $('.l-header__hamburger').click(function(){
+$(function () {
+  $('.l-header__hamburger').click(function () {
     $('.l-header__hamburger, .l-header__menu').toggleClass('active');
   });
 });
 
 
 
-// カルーセル
+// スムーススクロール
 $('#page-link a[href*="#"]').click(function () {
-  //全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
   var elmHash = $(this).attr('href');
-  //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
   var pos = $(elmHash).offset().top;
-  //idの上部の距離を取得
-  $('body,html').animate({scrollTop: pos}, 500);
-  //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  $('body,html').animate({ scrollTop: pos }, 500);
   $('.l-header__menu').toggleClass('active');
   return false;
-  // メニューのリンクをクリックしたらページを閉じる
 });
 
