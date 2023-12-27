08914a708209063db7f5076f5813281fef99f70f
sakigig <o0daifuku0o@gmail.com>
2023-12-27 11:36:40 +0900

    js修正（SP画面微調整）

--- src/js/app.js
+++ src/js/app.js
@@ -12,8 +12,6 @@ $(function () {
 $('#c-slider').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
-  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
   responsive: [
     {
       breakpoint: 768,
