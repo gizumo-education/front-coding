f1f2d6a262560377441c088c9b46254de4eb1396
Yusaku0329 <nakamura.rizumu.nukesaku74@gmail.com>
2024-01-24 12:18:59 +0900

    add:新着求人セクションにスライドショーを追加

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,21 @@
 import $ from "jquery";
+import "slick-carousel";
 
-$(function() {
-  console.log('環境構築完了');
-})
+$(function () {
+  console.log("環境構築完了");
+
+  $(".slide-items").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
