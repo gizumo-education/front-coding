a91349f6157a82e2bac61ff291f979cfecb9f50e
Yusaku0329 <nakamura.rizumu.nukesaku74@gmail.com>
2024-01-25 17:18:04 +0900

    fix:微細なスタイルの修正

--- src/js/app.js
+++ src/js/app.js
@@ -20,10 +20,10 @@ $(function () {
   });
 
   $('a[href^="#"]').click(function () {
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top;
-    var speed = 500;
+    const href = $(this).attr("href");
+    const target = $(href == "#" || href == "" ? "html" : href);
+    const position = target.offset().top;
+    const speed = 500;
     $("html, body").animate(
       {
         scrollTop: position,
