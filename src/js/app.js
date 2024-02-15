8c6ebe733462e7134cceaf5309ce11a4470199d7
ataruwatanabe <atarunba15@gmail.com>
2024-02-15 15:36:06 +0900

    feat:ハンバーガーメニューの実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,18 @@
 import $ from "jquery";
 
-$(function() {
-  console.log('環境構築完了');
+// ハンバーガーメニュー
+$(function () {
+  const headerHamburger = $('#js-hamburger');
+  const headerNav = $('#js-sp-nav');
+  headerHamburger.on('click', function () {
+    headerHamburger.toggleClass('active');
+    headerNav.toggleClass('active');
+
+    if ($("body").css("overflow") === "hidden") {
+      console.log('second')
+      $("body").css({ overflow: "" });
+    } else {
+      $("body").css({ overflow: "hidden" });
+    }
+  });
 })
