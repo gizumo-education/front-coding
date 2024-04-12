85e3d65df35c28e0e9e78da10e407427114c0aba
しゅんたろう <s.inai@ArkiBoocaM.local>
2024-04-12 11:37:26 +0900

    add: Swiper導入途中

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,6 @@
-import $ from "jquery";
+// import { Swiper } from "./library/swiper-bundle.min";
 
-$(function() {
-  console.log('環境構築完了');
-})
+const swiper = new Swiper(".swiper", {});
 
 window.onload = function () {
   const hamburger = document.getElementById('hamburger');
@@ -11,8 +9,45 @@ window.onload = function () {
 
   hamburger.addEventListener('click', function () {
     spMenu.classList.toggle('open');
-    hamburgerLine.classList.toggle('open');
+    hamburgerLine.forEach((line) => line.classList.toggle('open'));
+
+    var body = document.body;
+    var bodyStyle = window.getComputedStyle(body);
+    if (bodyStyle.overflow === "hidden") {
+      body.style.height = "";
+      body.style.overflow = "";
+    } else {
+      body.style.height = "100%";
+      body.style.overflow = "hidden";
+    }
+  });
+
+  const headerHeight = document.querySelector('header').offsetHeight;
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const href = anchor.getAttribute('href');
+      const target = document.getElementById(href.replace('#', ''));
+      const targetPosition = target.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+      const hamburger = document.getElementById('hamburger');
+      const hamburgerLine = hamburger.querySelectorAll('span');
+      const spMenu = document.getElementById('spMenu');
+      var body = document.body;
+      var bodyStyle = window.getComputedStyle(body);
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+      spMenu.classList.toggle('open');
+      hamburgerLine.forEach((line) => line.classList.toggle('open'));
+      if (bodyStyle.overflow === "hidden") {
+        body.style.height = "";
+        body.style.overflow = "";
+      } else {
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+      }
+    });
   });
 };
-
-// new Splide( '.splide' ).mount()
