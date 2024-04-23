c52b1ee29a1c3a8d1cf22ea1663a4c52c1f1ea50
towa.isemura <towa.isemura.0803@gmail.com>
2024-04-23 20:28:30 +0900

    4/23確認依頼

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,12 @@ import $ from "jquery";
 $(function() {
   console.log('環境構築完了');
 })
+
+const btn = document.querySelector('#hamburger_btn');
+const nav = document.querySelector('#hamburger_nav');
+
+// btn.addEventListener('click', function () {
+//   console.log('ok!');
+// });
+
+
