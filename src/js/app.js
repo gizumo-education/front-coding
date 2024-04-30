58caf6fd1ac846cde5393393ea5e549607c219e6
towa.isemura <towa.isemura.0803@gmail.com>
2024-04-30 21:08:12 +0900

    4/30確認依頼

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,25 @@
 import $ from "jquery";
+import 'slick-carousel'
 
-$(function() {
 
+$(function () {
+  $('#slider').slick({
+    arrows: true,
+  });
 });
 
 document.querySelector('.l-header__hamburger').addEventListener('click', function(){
   this.classList.toggle('active');
   document.querySelector('.l-header__hamburger_menu').classList.toggle('active');
 });
+
+
+$('#hamburger_menu a[href*="#"]').click(function () {
+
+});
+
+
+
 
 
 
