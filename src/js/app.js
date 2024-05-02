44b418125afaf59a3272f39570e8c6879ede3e5b
ryo-kihara <work1008.rk@gmail.com>
2024-05-02 11:26:29 +0900

    スライダー・ページ内リンク・ハンバーガーメニュースタイル修正

--- src/js/app.js
+++ src/js/app.js
@@ -3,24 +3,46 @@ import 'slick-carousel'
 
 
 $(function () {
+  // ハンバーガーメニュー
+  $('.l-header__hamburger').on('click', function () {
+    $(this).toggleClass('active');
+    $('.l-header__hamburger_menu').toggleClass('active');
+
+    if ($('body').hasClass('no_scroll')) {
+      $('body').removeClass('no_scroll');
+      return;
+    }
+    $('body').addClass('no_scroll');
+  });
+
+  // ページ内リンク
+  $('a[href^="#"]').on('click', function () {
+    const href = $(this).attr('href');
+    const target = $(href == "#" || href == "" ? "html" : href);
+    const height = $('.l-header').outerHeight();
+    const position = target.offset().top - height;
+    const speed = 500;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+
+  // ハンバーガーメニュー内リンク
+  $('.l-header__hamburger_menu a').on('click', function () {
+    $('.l-header__hamburger_menu').toggleClass('active');
+    $('.l-header__hamburger').toggleClass('active');
+    $('body').removeClass('no_scroll');
+  });
+
+  // スライダー
   $('#slider').slick({
-    arrows: true,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   });
 });
-
-document.querySelector('.l-header__hamburger').addEventListener('click', function(){
-  this.classList.toggle('active');
-  document.querySelector('.l-header__hamburger_menu').classList.toggle('active');
-});
-
-
-$('#hamburger_menu a[href*="#"]').click(function () {
-
-});
-
-
-
-
-
-
-
