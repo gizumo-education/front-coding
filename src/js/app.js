9f38b1df7f4909b03d2a7cb42d96c83319df19f4
yusuke0927 <gorirando0927@gmail.com>
2024-05-06 16:50:14 +0900

    レスポンシブで画面崩れが発生している

--- src/js/app.js
+++ src/js/app.js
@@ -7,8 +7,24 @@ $(function() {
 
 $(function () {
   $('.slick-recruitment').slick({
-    prevArrow: '<img src="./img/icon/news-page-arrow.png"></img>',
-    nextArrow: '<img src="./img/icon/news-page-arrow.png"></img>',
+    prevArrow: '<img src="./img/icon/news-page-arrow.png" alt="スクロールボタン左矢印" class="p-scloll__size"></img>',
+    nextArrow: '<img src="./img/icon/news-page-arrow.png" alt="スクロールボタン左矢印" class="p-scloll__size__second"></img>',
     slidesToShow: '4',
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+        slidesToShow: 1,
+        },
+      },
+    ],
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+        slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
