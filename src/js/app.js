15c68a8d27261d47c58b31f8727469a717b985ea
yusuke0927 <gorirando0927@gmail.com>
2024-05-18 15:39:00 +0900

    ①スライダー②ハンバーガーボタン③SPページ内リンク実装完了記録

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,35 @@ $(function() {
   console.log('環境構築完了');
 })
 
+$(function() {
+  //ハンバーガーボタン⇨メニュースクロール
+  $('.l-header__hamburger').on('click', function() {
+    $('.l-header__hamburger').toggleClass('open-btn');
+    $('.header-nav').toggleClass('open-menu');
+    
+    if ($("body").css("overflow") === "hidden") {
+      $("body").css({ height: "", overflow: "" });
+    } else {
+      $("body").css({ height: "100%", overflow: "hidden" });
+    }
+  });
+
+  //SPページ内リンク遷移
+  $(function(){
+    $('a[href^="#"]').on('click', function(){
+      var adjust = -80;
+      var speed = 400;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top + adjust;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+    });
+  });
+});
+
+//スライダー
+
 $(function () {
   $('.slick-recruitment').slick({
     prevArrow: '<img src="./img/icon/news-page-arrow.png" alt="スクロールボタン左矢印" class="p-scloll__size"></img>',
@@ -14,7 +43,7 @@ $(function () {
       {
         breakpoint: 1199,
         settings: {
-        slidesToShow: 1,
+        slidesToShow: 4,
         },
       },
     ],
