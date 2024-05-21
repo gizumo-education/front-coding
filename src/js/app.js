7db42f4a3763cc9ff6441f25e315d43901d4998d
井上晴矢 <inoueharuya@inoueharuyanonotobukkukonpyuta.local>
2024-05-21 22:54:26 +0900

    スライドショーの実装

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,13 @@ import $ from "jquery";
 
 $(function() {
   console.log('環境構築完了');
-})
+});
+
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 4,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
