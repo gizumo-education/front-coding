d421b2251e80f8e086766e9512623ca80747330e
tatsuhiroto615 <tatsuhi.revolution@gmail.com>
2024-06-07 17:56:28 +0900

    スライダー及びメニュー作成

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,11 @@ import 'slick-carousel'
 $(function() {
   $('#jobs-slider').slick()
 })
+
+const ham = document.querySelector('#js-hamburger');
+const nav = document.querySelector('#js-nav');
+
+ham.addEventListener('click', function() { 
+  ham.classList.toggle('active'); 
+  nav.classList.toggle('active');
+});
