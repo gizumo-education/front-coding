46046657cca584c7ac01fa927987e557fe3de464
atsuikeda <ike.ke.no.ke@gmail.com>
2024-06-14 14:52:49 +0900

    JS機能実装　レビュー内容の修正

--- src/js/app.js
+++ src/js/app.js
@@ -36,8 +36,8 @@ $(function () {
       slidesToShow: 4,
       slidesToScroll: 1,
       arrows: true,
-      // prevArrow: '<div class="slide-arrow prev-arrow"></div>',
-      // nextArrow: '<div class="slide-arrow next-arrow"></div>',
+      prevArrow: '<div class="slide-arrow prev-arrow"></div>',
+      nextArrow: '<div class="slide-arrow next-arrow"></div>',
       responsive: [
         {
           breakpoint: 1199,
