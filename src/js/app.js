18741c9c299c6900f76aede1865be05ac7ed9120
asuka_yamate <kutianfeiniao@gmail.com>
2024-06-17 12:55:56 +0900

    SP_ハンバーガーメニュー押下でバツに変更

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,11 @@ import $ from "jquery";
 $(function() {
   console.log('環境構築完了');
 })
+
+$(function(){
+  
+  $(".l-header__wrapper").click(function() {
+      $(this).toggleClass("l-header__close");
+  });
+
+});
