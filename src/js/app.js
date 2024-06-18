ccd889ebf231326596a3304fc3beba61f69e6b87
asuka_yamate <kutianfeiniao@gmail.com>
2024-06-19 00:47:23 +0900

    SP_ハンバーガーメニュー押下でメニュー表示と背景のスクロール制限

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,27 @@
 import $ from "jquery";
 
-$(function() {
+$(function () {
   console.log('環境構築完了');
 })
 
-$(function(){
-  
-  $(".l-header__wrapper").click(function() {
-      $(this).toggleClass("l-header__close");
+//メニュー開閉時のハンバーガー
+$(function () {
+  $(".l-header__wrapper").click(function () {
+    $(this).toggleClass("l-header__close");
   });
+});
 
+$(function () {
+  // ハンバーガーメニューのクリックイベント
+  $(".l-header__wrapper").click(function () {
+    $(".l-header__wrapper").toggleClass("is-active");
+    $("#nav").toggleClass("is-active");
+  });
+});
+
+//メニュー表示時は背景がスクロールしない
+$(document).ready(function () {
+  $('.l-header__wrapper').click(function () {
+    $('body').toggleClass('l-header__overflow');
+  });
 });
