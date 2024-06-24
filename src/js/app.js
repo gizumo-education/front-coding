34f5d2a6ddce4152a7d43f74cb1dc8b6ae90f392
asuka_yamate <kutianfeiniao@gmail.com>
2024-06-24 12:08:37 +0900

    スライダーのレイアウトについてメンターさんに質問

--- src/js/app.js
+++ src/js/app.js
@@ -10,7 +10,7 @@ $(function () {
   $(".l-header__wrapper").click(function () {
     $(this).toggleClass("l-header__close");
   });
-});
+
 
 $(function () {
   // ハンバーガーメニューのクリックイベント
@@ -26,27 +26,28 @@ $(document).ready(function () {
     $('body').toggleClass('l-header__overflow');
   });
 
-//スライドショー
-  $(document).ready(function () {
-    console.log('成功');
-    $(".u-container__position--job").slick({
-      arrows: true
-    });
-  });
+  //スライドショー
+//   $(document).ready(function () {
+//     console.log('成功');
+//     $(".u-container__position--job").slick({
+//       arrows: true
+//     });
+//   });
 });
 
 //スライドショー
-$(function () {
-  console.log('成功２');
-  $('.l-inner__job--container').slick({
-    infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      arrows: true,
-      prevArrow: '<div class="slide-arrow prev-arrow"></div>',
-      nextArrow: '<div class="slide-arrow next-arrow"></div>'
-  });
-});
+// $(function () {
+//   console.log('成功２');
+//   $('.l-inner__job--container').slick({
+//     infinite: true,
+//       slidesToShow: 4,
+//       slidesToScroll: 1,
+//       arrows: true,
+//       appendArrows: $('.c-button__box'),
+//       prevArrow: '<div class="slide-arrow prev-arrow"></div>',
+//       nextArrow: '<div class="slide-arrow next-arrow"></div>'
+//   });
+// });
 
 //SPメニューを押下で該当セクションンにスムーススクロール
 function linkscroll(target) {
@@ -67,4 +68,72 @@ $('#link__id4').on(function () {
 });
 $('#link__id5').on(function () {
   linkscroll('#target__id5');
+});
+
+
+// スライドショー
+$('.l-inner__job--container').not('slick-initialized').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  infinite: true,
+  responsive: [{
+    breakpoint: 1199,
+    settings: {
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    },
+  },
+    {
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+  },
+  ]
+});
+
+// $(function() {
+//   $('body *').removeAttr('style');
+// });
+
+// $('body *').removeAttr('width');
+// 		$('body *').removeAttr('height');
+// $(".slick-slide .slick-current .slick-active").removeAttr("width");
+// slick-slide slick-current slick-active
+
+// $(function() {
+//   let element = document.querySelector('div.slick-slide.slick-active');
+//   element.style.width = null;
+// });
+
+// $(function() {
+// element.style.width = '280px';
+//     let element = document.querySelector('div.slick-slide.slick-active');
+//     element.style.removeProperty('width');
+// });
+
+$(function() {
+  // let element = document.querySelector('div.slick-slide.slick-active');
+  // element.style.width = '280px'; // 幅を280pxに設定
+  // element.style.removeProperty('width'); // 幅のプロパティを削除
+});
+// $(function() {
+//   let element = document.querySelector('div.slick-active');
+//   element.style.width = '280px'; // 幅を280pxに設定
+  // element.style.removeProperty('width'); // 幅のプロパティを削除
+// });
+
+$(function() {
+  let elements = document.querySelectorAll('div.slick-slide.slick-active');
+  elements.forEach(function(element) {
+    element.style.width = '280px';
+  });
+});
+// $(function() {
+//   let elements = document.querySelectorAll('div.slick-slide.slick-current.slick-active');
+//   elements.forEach(function(elements) {
+//     elements.style.width = '400px';
+//   });
+// });
 });
