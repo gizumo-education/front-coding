cfd5fa33b34238cbed6f16c0e695bcddd25f366b
Todatomoyuki <tomoyuki.jam1@gmail.com>
2024-08-27 11:33:44 +0900

    fix レビュー指摘箇所修正

--- src/js/app.js
+++ src/js/app.js
@@ -5,30 +5,26 @@ $(function() {
   console.log('環境構築完了');
 
   //メニュースクロールイン
-  const hamMenu = document.querySelector(".l-header__hamburger");
-  const navMenu = document.querySelector(".l-header__nav-menu");
-
-  hamMenu.addEventListener("click", (event) => {
-    hamMenu.classList.toggle('l-header__hamburger__active');
-    navMenu.classList.toggle('l-header__nav-menu__active');
-    if (document.querySelector('.l-header__nav-menu__active')) {
-      document.body.style.overflow = 'hidden';
+  $('.l-header__hamburger').on('click', function () {
+    $(this).toggleClass('active');
+    $('.l-header__nav-menu').toggleClass('active');
+    if ($('body').css('overflow') === 'hidden') {
+      $('body').css({ overflow: '' });
     } else {
-      document.body.style.overflow = 'auto';
+      $('body').css({ overflow: 'hidden' });
     }
   })
 
   //該当セクションスクロール
   $('a[href^="#"]').click(function () {
     const speed = 600;
-    let href = $(this).attr("href");
-    let target = $(href === "#" || href === "" ? "html" : href);
-    let position = target.offset().top;
+    const href = $(this).attr("href");
+    const target = $(href === "#" || href === "" ? "html" : href);
+    const position = target.offset().top;
     $("body,html").animate({ scrollTop: position - 60 }, speed, "swing");
-    hamMenu.classList.remove('l-header__hamburger__active');
-    navMenu.classList.remove('l-header__nav-menu__active');
+    $('.l-header__hamburger').removeClass('active');
+    $('.l-header__nav-menu').removeClass('active');
     document.body.style.overflow = 'auto';
-    return false;
   });
 
   //無限スライダー
