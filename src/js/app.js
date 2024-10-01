705d63d39254b9fc8778e1209f595af763d34141
itowataru2024 <itawatru2@gmail.com>
2024-10-02 07:21:35 +0900

    JS実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,47 @@
 import $ from "jquery";
+import 'slick-carousel'
+
 
 $(function() {
-  console.log('環境構築完了');
+  $('.p-joblistings__job-openings').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slide-arrow prev-arrow"></div>',
+    nextArrow: '<div class="slide-arrow next-arrow"></div>',
+    responsive: [
+      {
+        breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+      }
+    ]
+  })
+  $(function(){
+    $('.l-header__hamburger').on("click",function(){
+      $('.l-header__hamburger, .l-header__slide-menu').toggleClass('active');
+    });
+  });
 })
+
+$(function () {
+  $(".l-header__hamburger").on("click",function () {
+    if ($("body").css("overflow") === "hidden") {
+
+      $("body").css({ height: "", overflow: "" });
+   
+    } else {
+      $("body").css({ height: "100%", overflow: "hidden" });
+
+    }
+  });
+});
+
+$(function() {
+  $('#"l-header__nav').on('click', function(event) {
+    $('#nav-input').prop('checked', false);
+  });
+});
