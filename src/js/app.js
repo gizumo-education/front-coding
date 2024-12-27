ed5d3e19e0148ee6e106d81651f6d49c743cd792
snmtnnk <shinmotonanako@gmail.com>
2024-12-27 11:38:31 +0900

    add: （JS）スライダー実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,20 @@
 import $ from "jquery";
+import 'slick-carousel';
 
-$(function() {
+$(function () {
   console.log('環境構築完了');
 })
+
+window.addEventListener('load', () => {
+  $('.slick').slick({
+    arrows: true,
+    slidesToShow: 4,
+    infinite: true,
+    swipeToSlide: false,
+    accessibility: true,
+    appendArrows: $('.is-relative'),
+    prevArrow: '<div class="prev-arrow"></div>',
+    nextArrow: '<div class="next-arrow"></div>',
+    variableWidth: true,
+  });
+});
