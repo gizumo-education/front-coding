e785325c95788736fe1f8d4cedfea6efafb0a80f
TOMOYA <jingcaoq@gmail.com>
2025-01-08 10:45:08 +0900

    ハンバーガーメニューベース実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,47 @@
 import $ from "jquery";
 
-$(function() {
+$(function () {
   console.log('環境構築完了');
 })
+
+
+
+
+const hamburger = document.querySelector('.l-header__hamburger');
+
+
+const headNav = document.querySelector('#js-nav');
+
+
+hamburger.addEventListener('click', () => {
+  if (hamburger.classList.contains("is-active")) {
+    hamburger.classList.remove('is-active');
+
+    headNav.classList.remove('is-active')
+
+  } else {
+    hamburger.classList.add('is-active');
+
+    headNav.classList.add('is-active')
+  }
+
+
+  headNav.addEventListener('click', () => {
+    hamburger.classList.remove('is-active');
+    headNav.classList.remove('is-active')
+  });
+
+});
+
+
+// const left = document.querySelector('.p-jobslist__arrow-left');
+
+// const right = document.querySelector('.p-jobslist__arrow-right');
+
+// right.addEventListener('click', () => {
+//   if
+
+// });
+
+// $('.p-jobslist__arrow-left').slick();
+
