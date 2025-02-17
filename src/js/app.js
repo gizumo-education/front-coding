aa541f25851a04b35ecc965222445652f6150c67
unknown <s.tsuchida07@gmail.com>
2025-02-17 16:40:37 +0900

    5枚表示のスライダー実装（矢印未完了）

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
 import $ from "jquery";
-import 'slick-carousel'
+import "slick-carousel";
 
 $(function() {
   console.log('環境構築完了');
 })
+
+  $('.p-main__recruit__container__wrapper').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+  });
