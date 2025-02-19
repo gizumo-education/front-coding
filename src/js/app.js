b27dc189c51fe835215a9f473ad4ee7ee0db4cd1
unknown <s.tsuchida07@gmail.com>
2025-02-19 14:38:03 +0900

    スリックスライダーのレスポンシブデザイン化実装

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,17 @@ import "slick-carousel";
 
 $(function() {
   console.log('環境構築完了');
-})
 
   $('.c-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    arrows: true,
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+      }
+    }],
     prevArrow: '<button class="c-prev-arrow"></button>',
     nextArrow: '<button class="c-next-arrow"></button>',
   });
+})
