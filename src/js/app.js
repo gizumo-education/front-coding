42d18a40b4d6f98251fccb79bf441b492a172662
natsukikokubu <natsukikokubu3@gmail.com>
2025-03-26 12:32:00 +0900

    add: hamburgerメニューのJS実装

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import $ from "jquery";
 
-$(function() {
-  console.log('環境構築完了');
-})
+$(function () {
+  $("#hamburger").on("click", function () {
+    $("#nav").toggleClass("is-active");
+  });
+});
