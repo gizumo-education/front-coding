e1f7fa4b6662f05713b968191b83d0639b36b3dd
natsukikokubu <natsukikokubu3@gmail.com>
2025-03-26 16:10:43 +0900

    add: バツボタン追加

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,9 @@ import $ from "jquery";
 $(function () {
   $("#hamburger").on("click", function () {
     $("#nav").toggleClass("is-active");
+    $("#span1").toggleClass("span1");
+    $("#span2").toggleClass("span2");
+    $("#span3").toggleClass("span3");
   });
   $(".p-nav__item__link").on("click", function () {
     $("#nav").removeClass("is-active");
