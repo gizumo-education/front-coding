1a5a913922b98f144fd2b1bdf8b77ed12d14157e
natsukikokubu <natsukikokubu3@gmail.com>
2025-03-27 17:08:30 +0900

    コンフリクト解消

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@ import $ from "jquery";
 import "slick-carousel";
 import "slick-carousel/slick/slick.css";
 
-<<<<<<< HEAD
 //ハンバーガーメニュー
 $(function () {
   $("#hamburger").on("click", function () {
@@ -33,8 +32,4 @@ $(function () {
     swipeToSlide: true,
     // variableWidth: true,
   });
-=======
-$(function () {
-  console.log("環境構築完了");
->>>>>>> develop_tachibana_natsuki
 });
