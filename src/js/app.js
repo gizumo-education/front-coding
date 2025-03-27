506829a83ef61f1472fa9f1b58fffda85c85279a
natsukikokubu <natsukikokubu3@gmail.com>
2025-03-27 10:42:08 +0900

    fix: ページ内スクロール後のハンバーガーメニューの挙動修正

--- src/js/app.js
+++ src/js/app.js
@@ -10,5 +10,9 @@ $(function () {
   });
   $(".p-nav__item__link").on("click", function () {
     $("#nav").removeClass("is-active");
+    $("#span1").removeClass("span1");
+    $("#span2").removeClass("span2");
+    $("#span3").removeClass("span3");
+    $("#body").removeClass("fixed");
   });
 });
