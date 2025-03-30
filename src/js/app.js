9fcdde17b73319ef04bbc10ba6052adb9091f8a4
natsukikokubu <natsukikokubu3@gmail.com>
2025-03-31 06:04:21 +0900

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick.css";
 //ハンバーガーメニュー
 $(function () {
   $("#hamburger").on("click", function () {
+    var speed = 300;
     $("#nav").toggleClass("is-active");
     $("#span1").toggleClass("span1");
     $("#span2").toggleClass("span2");
