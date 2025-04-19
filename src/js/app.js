7e6c75e35fc1d6769a6731f24a10b8dc1fe9be84
Takeru Kakuta <kakuta19960915@gmail.com>
2025-04-19 11:15:44 +0900

    スムーススクロール機能を実装

--- src/js/app.js
+++ src/js/app.js
@@ -47,4 +47,19 @@ $(window).on('load', function() {
     $('.l-header__sp-menu').removeClass('is-open');
   });
 
+  $('.l-header__sp-menu a[href^="#"]').on('click', function(e) {
+    e.preventDefault(); 
+
+    const speed = 800; 
+    const href = $(this).attr('href'); 
+    const $target = $(href === "#" || href === "" ? 'html' : href);
+    const targetOffset = $target.offset().top; 
+    const headerHeight = $('.l-header').outerHeight(); 
+    const position = targetOffset - headerHeight;
+
+    $('html, body').animate({ scrollTop: position }, speed);
+    $('.l-header__hamburger').removeClass('is-open');
+    $('.l-header__sp-menu').removeClass('is-open');
+  });
+
 });
