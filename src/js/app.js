d09831d7a746d96363011f048d9f0f0d3c76090c
Takeru Kakuta <kakuta19960915@gmail.com>
2025-04-24 14:57:45 +0900

    繰り返し取得しているdomを変数化

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,18 @@ import 'slick-carousel/slick/slick.min.js';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-$(window).on('load', function() {
+$(window).on('load', function () {
   const $slider = $('.p-job__new-job');
-  
+  const $hamburger = $('.l-header__hamburger');
+  const $spMenu = $('.l-header__sp-menu');
+  const $body = $('body');
+  const $header = $('.l-header');
+
   $slider.slick({
     dots: true,
-    arrows: false, 
+    arrows: false,
     slidesToShow: 4,
-    infinite: true, 
+    infinite: true,
     slidesToScroll: 1,
     responsive: [
       {
@@ -34,42 +38,36 @@ $(window).on('load', function() {
     $slider.slick('slickNext');
   });
 
-  $('.l-header__hamburger').on('click', function (e) {
+  $hamburger.on('click', function (e) {
     e.stopPropagation();
     $(this).toggleClass('is-open');
-    $('.l-header__sp-menu').toggleClass('is-open');
-
-    if ($(this).hasClass('is-open')) {
-      $('body').addClass('is-fixed');
-    } else {
-      $('body').removeClass('is-fixed');
-    }
+    $spMenu.toggleClass('is-open');
+    $body.toggleClass('is-fixed', $(this).hasClass('is-open'));
   });
 
   $(document).on('click', function (e) {
-    const isClickInsideHamburgerOrMenu = $(e.target).closest('.l-header__hamburger, .l-header__sp-menu').length > 0 
+    const isClickInside = $(e.target).closest('.l-header__hamburger, .l-header__sp-menu').length > 0;
 
-    if (isClickInsideHamburgerOrMenu) {
-      $('.l-header__hamburger').removeClass('is-open');
-      $('.l-header__sp-menu').removeClass('is-open'); 
-      $('body').removeClass('is-fixed');
+    if (!isClickInside) {
+      $hamburger.removeClass('is-open');
+      $spMenu.removeClass('is-open');
+      $body.removeClass('is-fixed');
     }
   });
 
-  $('.l-header__sp-menu a[href^="#"]').on('click', function (e) {
+  $spMenu.find('a[href^="#"]').on('click', function (e) {
     e.preventDefault();
 
     const speed = 800;
     const href = $(this).attr('href');
     const $target = $(href === '#' || href === '' ? 'html' : href);
     const targetOffset = $target.offset().top;
-    const headerHeight = $('.l-header').outerHeight();
-    const position = targetOffset - headerHeight;
+    const position = targetOffset - $header.outerHeight();
 
     $('html, body').animate({ scrollTop: position }, speed);
 
-    $('.l-header__hamburger').removeClass('is-open');
-    $('.l-header__sp-menu').removeClass('is-open');
-    $('body').removeClass('is-fixed');
+    $hamburger.removeClass('is-open');
+    $spMenu.removeClass('is-open');
+    $body.removeClass('is-fixed');
   });
 });
