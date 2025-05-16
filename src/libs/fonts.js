3b6d636ecc997647e826cce1e0d0c0302846dd03
keisuke3 <ishii@gizumo-inc.jp>
2025-05-16 12:51:31 +0900

    chore: font-family設定追加

---
+++ src/libs/fonts.js
@@ -0,0 +1,8 @@
+import { Noto_Sans_JP } from "next/font/google";
+
+export const notoSansJP = Noto_Sans_JP({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+    variable: "--font-noto-sans-jp",
+    display: "swap",
+});
