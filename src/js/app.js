8f4d7a715789df043c4edb99fd01a080cf59896a
horike <horike@gizumo-admin-12noMacBook-Pro.local>
2025-07-07 15:16:21 +0900

    add:カルーセル機能実装

--- src/js/app.js
+++ src/js/app.js
@@ -13,5 +13,52 @@ $(function() {
   .children()
   .on('click',function() {
     scrollToTheSection($(this))
+    toggleMenu()
+    toggleHamburgerMenuButton($("#hamburger-menu"))
+  })
+  // 画面幅を取得
+  let windowWidth = $(window).width();
+  let tabFontSize = windowWidth / 12; // タブのフォントサイズを算出
+
+  // Swiperのbreakpointsプロパティ値
+  const breakpoints = {
+    0: {
+      slidesPerView: 1,
+    },
+    767: {
+      slidesPerView: 4,
+      spaceBetween: tabFontSize * 0.2, // タブのフォントサイズをspaceBetweenに設定
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  };
+
+  // カルーセルスライダー
+  const swiper = new Swiper('.p-job-list__items', {
+    loop: true,
+    breakpoints,
+  })
+
+  $('.p-job-list__back-button')
+  .on('click', function() {
+    swiper.slidePrev();
+  })
+
+  $('.p-job-list__forward-button')
+  .on('click', function() {
+    console.log(swiper.slideNext());
+    swiper.slideNext();
+  })
+
+  $(window).on('resize', function() {
+    // 画面幅を再取得
+    windowWidth = $(window).width();
+    // タブのフォントサイズを再算出
+    tabFontSize = windowWidth / 12;
+    // レスポンシブ設定でspaceBetweenを再設定
+    swiper.params.breakpoints = breakpoints
+    swiper.update();
   })
 })
