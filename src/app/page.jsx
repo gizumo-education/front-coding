69cf7417b76e82b01bade8a01ff231b820079f6c
高橋　輝 <hikaru0947@gmail.com>
2025-07-08 00:16:19 +0900

    [add]基本コーディング前半セクション pageファイル追加

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
 import { Header } from '@/components/layout'
+import { Keyvisual } from '@/components/Project/Keyvisual'
+import { Form } from '@/components/Project/Form'
+import { News } from '@/components/Project/news'
+import { Group1 } from '@/components/Project/Group1'
 
 const Top = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Keyvisual />
+        <Form />
+        <News />
+        <Group1 />
+      </main>
       <footer></footer>
     </>
   )
