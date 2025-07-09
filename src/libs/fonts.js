31bccc9b88d20667767312e4d625263decba197e
keisuke3 <ishii@gizumo-inc.jp>
2025-07-09 12:20:16 +0900

    chore: 使用フォント追加

--- src/libs/fonts.js
+++ src/libs/fonts.js
@@ -1,8 +1,29 @@
-import { Noto_Sans_JP } from 'next/font/google'
+import { Noto_Sans_JP, Inter, Shippori_Mincho, DM_Sans } from 'next/font/google'
 
 export const notoSansJP = Noto_Sans_JP({
   weight: ['400', '700'],
   subsets: ['latin'],
   variable: '--font-noto-sans-jp',
+  display: 'swap',
+})
+
+export const inter = Inter({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+})
+
+export const shipporiMincho = Shippori_Mincho({
+  weight: ['400', '700'],
+  subsets: ['japanese'],
+  variable: '--font-shippori-mincho',
+  display: 'swap',
+})
+
+export const dmSans = DM_Sans({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-dm-sans',
   display: 'swap',
 })
