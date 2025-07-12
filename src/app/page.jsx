fcf159caa7cfba37ea5272432fc48ef2f1dde735
高橋　輝 <hikaru0947@gmail.com>
2025-07-12 17:59:48 +0900

    [add]基本コーディング 後半セクション FeaturesとFlowStepsとHelpfulContentsとJobFormページ追加

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,10 @@ import { Keyvisual } from '@/components/Project/Keyvisual'
 import { Form } from '@/components/Project/Form'
 import { News } from '@/components/Project/news'
 import { MainVisial } from '@/components/Project/MainVisial'
+import { Features } from '@/components/Project/Features'
+import { FlowSteps } from '@/components/Project/FlowSteps'
+import { HelpfulContents } from '@/components/Project/HelpfulContents'
+import { JobForm } from '@/components/Project/JobForm'
 
 const Top = () => {
   return (
@@ -13,6 +17,10 @@ const Top = () => {
         <Form />
         <News />
         <MainVisial />
+        <Features />
+        <FlowSteps />
+        <HelpfulContents />
+        <JobForm />
       </main>
       <footer></footer>
     </>
