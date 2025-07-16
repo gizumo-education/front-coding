ef1f85d9caebd14643e95108c13b1bd7ad26a4d1
高橋　輝 <hikaru0947@gmail.com>
2025-07-16 12:07:26 +0900

    [add]基本コーディング レスポンシブセクション FlowStepsセクションSP実装追加

--- src/components/Project/FlowSteps/index.jsx
+++ src/components/Project/FlowSteps/index.jsx
@@ -11,16 +11,26 @@ export const FlowSteps = () => {
               STEP
               <br />1
             </span>
-            <img
-              src='./img/icon/FlowStep-entry-icon.png'
-              alt='エントリーアイコン'
-              className={styles['entry-icon']}
-              width='102'
-              height='100'
-            />
+            <picture>
+              <source
+                srcSet='./img/icon/FlowStep-entry-icon.png'
+                media='(max-width: 767px)'
+              />
+              <img
+                src='./img/icon/FlowStep-entry-icon.png'
+                alt='エントリーアイコン'
+                className={styles['entry-icon']}
+                width='102'
+                height='100'
+              />
+            </picture>
             <h3 className={styles['step-title']}>エントリーシートの作成</h3>
             <p className={styles['step-desc']}>
-              ここにはテキストが入ります。ここにはテキストが入ります。ここにはテキストが入ります。こ
+              ここにはテキストが入ります。ここにはテキストが入ります。
+              <br className={styles['sp-text']} />
+              ここにはテキストが入ります。
+              <br className={styles['sp-text']} />
+              ここにはテキストが
             </p>
           </div>
           <div className={styles.triangle}></div>
@@ -29,20 +39,30 @@ export const FlowSteps = () => {
               STEP
               <br />2
             </span>
-            <img
-              src='./img/icon/FlowStep-path-icon.png'
-              alt='検索アイコン'
-              className={styles['path-icon']}
-              width='80'
-              height='80'
-            />
+            <picture>
+              <source
+                srcSet='./img/icon/FlowStep-path-icon.png'
+                media='(max-width: 767px)'
+              />
+              <img
+                src='./img/icon/FlowStep-path-icon.png'
+                alt='検索アイコン'
+                className={styles['path-icon']}
+                width='80'
+                height='80'
+              />
+            </picture>
             <h3
               className={`${styles['step-title']} ${styles['step-title--pickup']}`}
             >
               気になる企業のピックアップ
             </h3>
             <p className={styles['step-desc']}>
-              ここにはテキストが入ります。ここにはテキストが入ります。ここにはテキストが入ります。ここにはテキストが
+              ここにはテキストが入ります。ここにはテキストが入ります。
+              <br className={styles['sp-text']} />
+              ここにはテキストが入ります。
+              <br className={styles['sp-text']} />
+              ここにはテキストが
             </p>
           </div>
           <div className={styles.triangle}></div>
@@ -51,18 +71,28 @@ export const FlowSteps = () => {
               STEP
               <br />3
             </span>
-            <img
-              src='./img/icon/FlowStep-mail-icon.png'
-              alt='メールアイコン'
-              className={styles['mail-icon']}
-              width='107'
-              height='100'
-            />
+            <picture>
+              <source
+                srcSet='./img/icon/FlowStep-mail-icon.png'
+                media='(max-width: 767px)'
+              />
+              <img
+                src='./img/icon/FlowStep-mail-icon.png'
+                alt='メールアイコン'
+                className={styles['mail-icon']}
+                width='107'
+                height='100'
+              />
+            </picture>
             <h3 className={styles['step-title']}>複数企業に応募</h3>
             <p
               className={`${styles['step-desc']} ${styles['step-desc--text']}`}
             >
-              ここにはテキストが入ります。ここにはテキストが入ります。ここにはテキストが入ります。ここにはテキストが
+              ここにはテキストが入ります。ここにはテキストが入ります。
+              <br className={styles['sp-text']} />
+              ここにはテキストが入ります。
+              <br className={styles['sp-text']} />
+              ここにはテキストが
             </p>
           </div>
           <div className={styles.triangle}></div>
@@ -71,18 +101,28 @@ export const FlowSteps = () => {
               STEP
               <br />4
             </span>
-            <img
-              src='./img/icon/FlowStep-star-icon.png'
-              alt='転職アイコン'
-              className={styles['star-icon']}
-              width='108'
-              height='113'
-            />
+            <picture>
+              <source
+                srcSet='./img/icon/FlowStep-star-icon.png'
+                media='(max-width: 767px)'
+              />
+              <img
+                src='./img/icon/FlowStep-star-icon.png'
+                alt='転職アイコン'
+                className={styles['star-icon']}
+                width='108'
+                height='113'
+              />
+            </picture>
             <h3 className={styles['step-title']}>内定・転職！</h3>
             <p
               className={`${styles['step-desc']} ${styles['step-desc--text2']}`}
             >
-              ここにはテキストが入ります。ここにはテキストが入ります。ここにはテキストが入ります。ここにはテキストが
+              ここにはテキストが入ります。ここにはテキストが入ります。
+              <br className={styles['sp-text']} />
+              ここにはテキストが入ります。
+              <br className={styles['sp-text']} />
+              ここにはテキストが
             </p>
           </div>
         </div>
