c76933ed1c5cced48ea172918d98bf1dd599f0a7
omochi <omochi@example.com>
2025-07-29 22:45:32 +0900

    sp セレクトフィールド内矢印の実装

--- src/app/_components/Recruitment/index.jsx
+++ src/app/_components/Recruitment/index.jsx
@@ -16,9 +16,18 @@ export const Recruitment = () => {
             働きたい職種・地域・希望給与を入れて検索！
           </p>
           <section className={styles['select-fields']}>
-            <section className={styles['select-field']}>医療事務</section>
-            <section className={styles['select-field']}>東京都</section>
-            <section className={styles['select-field']}>月給17万～</section>
+            <section className={styles['select-field']}>
+              医療事務
+              <section className={styles['triangle']}></section>
+            </section>
+            <section className={styles['select-field']}>
+              東京都
+              <section className={styles['triangle']}></section>
+            </section>
+            <section className={styles['select-field']}>
+              月給17万～
+              <section className={styles['triangle']}></section>
+            </section>
           </section>
           <section className={styles['input-field']}>
             キーワード・企業名・スキルなど
