9bc5549c90e2fec43f65caee9a52c608d0ea1e25
omochi <omochi@example.com>
2025-07-30 17:01:24 +0900

    sp Mainvisual 文章と装飾の実装

--- src/app/_components/Mainvisual/index.jsx
+++ src/app/_components/Mainvisual/index.jsx
@@ -4,12 +4,11 @@ export const Mainvisual = () => {
   return (
     <main className={styles['main']}>
       <section className={styles['main-visual']}>
-        <img
-          src='/images/メインビジュアル.png'
-          alt='メインビジュアル'
-          width='400'
-          height='75'
-        />
+        <div className={styles['main-visual-bg']}></div>
+        <section className={styles['main-visual-pra']}>
+          <h2>ギズジョブなら理想の職場がきっと見つかる</h2>
+          <h2>ギズジョブは完全無料の求職者向け求人サービスです。</h2>
+        </section>
       </section>
     </main>
   )
