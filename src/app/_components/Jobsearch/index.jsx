2bc9c0d4300c1ba942a0ae7e80732fd85532bdd5
omochi <omochi@example.com>
2025-07-30 18:53:00 +0900

    sp selectフィールド修正、矢印マークの実装

--- src/app/_components/Jobsearch/index.jsx
+++ src/app/_components/Jobsearch/index.jsx
@@ -5,28 +5,26 @@ export const Jobsearch = () => {
     <main className={styles['main']}>
       <section className={styles['inner']}>
         <section className={styles['work-flame']}>
-          <img
-            src='/images/お仕事検索.png'
-            alt='お仕事検索'
-            width='110'
-            height='20'
-            className={styles['work-title']}
-          />
-          <section className={styles['work-caption']}>
+          <h1 className={styles['work-title']}>お仕事検索</h1>
+          <p className={styles['work-caption']}>
             働きたい職種・地域・希望給与を入れて検索！
-          </section>
+          </p>
           <section className={styles['select-fields']}>
-            <div>
-              <select className={styles['select-field']}>
+            <div className={styles['select-field-container']}>
+              <select className={styles['select-field']} name='job'>
                 <option value='医療事務'>医療事務</option>
               </select>
             </div>
-            <select className={styles['select-field']}>
-              <option value='東京都'>東京都</option>
-            </select>
-            <select className={styles['select-field']}>
-              <option value='月給17万～'>月給17万～</option>
-            </select>
+            <div className={styles['select-field-container']}>
+              <select className={styles['select-field']}>
+                <option value='東京都'>東京都</option>
+              </select>
+            </div>
+            <div className={styles['select-field-container']}>
+              <select className={styles['select-field']}>
+                <option value='月給17万～'>月給17万～</option>
+              </select>
+            </div>
           </section>
           <input
             className={styles['input-field']}
