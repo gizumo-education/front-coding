a5a23166003f1a2c64bee2a97b24fdad9931b49d
omochi <omochi@example.com>
2025-07-31 16:22:08 +0900

    sp セレクトボックスデフォルト値設定修正

--- src/app/_components/Jobsearch/index.jsx
+++ src/app/_components/Jobsearch/index.jsx
@@ -11,7 +11,11 @@ export const Jobsearch = () => {
           </p>
           <section className={styles['select-fields']}>
             <div className={styles['select-field-container']}>
-              <select className={styles['select-field']} name='job'>
+              <select
+                className={styles['select-field']}
+                name='job'
+                value='医療事務'
+              >
                 <option value='医療事務'>医療事務</option>
                 <option value='調剤事務'>調剤事務</option>
                 <option value='診療情報管理士'>診療情報管理士</option>
@@ -22,26 +26,22 @@ export const Jobsearch = () => {
               </select>
             </div>
             <div className={styles['select-field-container']}>
-              <select className={styles['select-field']}>
-                <option value='東京都'>茨城県</option>
+              <select className={styles['select-field']} value='東京都'>
+                <option value='茨城県'>茨城県</option>
                 <option value='神奈川県'>神奈川県</option>
                 <option value='群馬県'>群馬県</option>
                 <option value='埼玉県'>埼玉県</option>
                 <option value='千葉県'>千葉県</option>
-                <option value='東京都' selected>
-                  東京都
-                </option>
+                <option value='東京都'>東京都</option>
                 <option value='栃木県'>栃木県</option>
               </select>
             </div>
             <div className={styles['select-field-container']}>
-              <select className={styles['select-field']}>
+              <select className={styles['select-field']} value='月給20万～'>
                 <option value='月給17万～'>月給17万～</option>
                 <option value='月給18万～'>月給18万～</option>
                 <option value='月給19万～'>月給19万～</option>
-                <option value='月給20万～' selected>
-                  月給20万～
-                </option>
+                <option value='月給20万～'>月給20万～</option>
                 <option value='月給21万～'>月給21万～</option>
                 <option value='月給22万～'>月給22万～</option>
                 <option value='月給23万～'>月給23万～</option>
