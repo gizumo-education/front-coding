bf5eeaabe73bd097a550fd06a502969b03745596
omochi <omochi@example.com>
2025-08-01 00:12:45 +0900

    sp コラムボタンの実装、クラス名の変更

--- src/app/_components/Contents/index.jsx
+++ src/app/_components/Contents/index.jsx
@@ -4,30 +4,33 @@ export const Contents = () => {
   return (
     <main className={styles['main']}>
       <section className={styles['inner']}>
-        <h1 className={styles['job-changes-title']}>転職お役立ちコンテンツ</h1>
+        <h1 className={styles['job-changes-contents']}>
+          転職お役立ちコンテンツ
+        </h1>
 
         <div className={styles['job-change-container']}>
-          <div className={styles['job-change-title']}>
+          <div className={styles['content-title']}>
             <h2>転職マニュアル</h2>
           </div>
-          <div className={styles['job-change-p']}>
+          <div className={styles['content-text']}>
             <p>転職活動の基礎知識、面接対策など転職活動のコツをお教えします</p>
           </div>
         </div>
         <div className={styles['job-change-container']}>
-          <div className={styles['job-change-container-cover']}>
-            <h2 className={styles['job-change-title']}>医療事務業界の豆知識</h2>
-            <p className={styles['job-change-p']}>
-              スキルアップ・労働環境、給与事情など知って得する業界の豆知識を公開
-            </p>
-          </div>
+          <h2 className={styles['content-title']}>医療事務業界の豆知識</h2>
+          <p className={styles['content-text']}>
+            スキルアップ・労働環境、給与事情など知って得する業界の豆知識を公開
+          </p>
         </div>
         <div className={styles['job-change-container']}>
-          <h2 className={styles['job-change-title']}>転職マニュアル</h2>
-          <p className={styles['job-change-p']}>
+          <h2 className={styles['content-title']}>転職マニュアル</h2>
+          <p className={styles['content-text']}>
             転職活動の豆知識、面接対策など転職のコツをお教えします
           </p>
         </div>
+        <button className={styles['column-button']}>
+          医療事務コラムをもっと見る
+        </button>
       </section>
     </main>
   )
