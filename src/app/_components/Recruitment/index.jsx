5ff5f6a9dd7db066d74dc77d8e13bca3cd86ddce
omochi <omochi@example.com>
2025-08-06 21:52:38 +0900

    Article　スライダー実装

--- src/app/_components/Recruitment/index.jsx
+++ src/app/_components/Recruitment/index.jsx
@@ -1,3 +1,11 @@
+'use client'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
 import styles from './index.module.scss'
 
 export const Recruitment = () => {
@@ -17,128 +25,174 @@ export const Recruitment = () => {
                 <div className={styles['recruit-nav-arrow-2']}></div>
               </nav>
             </div>
-            <ul className={styles['recruit-container']}>
-              <li className={styles['recruit-content']}>
-                <img
-                  src='/images/新着求人画像.png'
-                  alt='新着求人画像'
-                  width='370'
-                  height='208'
-                  className={styles['recruit-img']}
-                />
-                <h1 className={styles['recruit-caption']}>
-                  平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
-                </h1>
-                <section>
-                  <div className={styles['recruit-info']}>
-                    <ul>
-                      <li className={styles['info-title']}>給与</li>
-                      <li className={styles['info-title']}>所在地</li>
-                      <li className={styles['info-title']}>最寄駅</li>
-                    </ul>
-                  </div>
-                  <div className={styles['recruit-info']}>
-                    <ul>
-                      <li className={styles['info-detail']}>月給148,000円~</li>
-                      <li className={styles['info-detail']}>三重県伊勢市</li>
-                      <li className={styles['info-detail']}>
-                        伊勢市駅 徒歩15分
-                      </li>
-                    </ul>
-                  </div>
-                </section>
-              </li>
-              <li className={styles['recruit-content']}>
-                <img
-                  src='/images/新着求人女性2.jpg'
-                  alt='新着求人画像2'
-                  width='370'
-                  height='208'
-                  className={styles['recruit-img']}
-                />
-                <h1 className={styles['recruit-caption']}>
-                  平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
-                </h1>
-                <section>
-                  <div className={styles['recruit-info']}>
-                    <ul>
-                      <li className={styles['info-title']}>給与</li>
-                      <li className={styles['info-title']}>所在地</li>
-                      <li className={styles['info-title']}>最寄駅</li>
-                    </ul>
-                  </div>
-                  <div className={styles['recruit-info']}>
-                    <ul>
-                      <li className={styles['info-detail']}>月給148,000円~</li>
-                      <li className={styles['info-detail']}>三重県伊勢市</li>
-                      <li className={styles['info-detail']}>
-                        伊勢市駅 徒歩15分
-                      </li>
-                    </ul>
-                  </div>
-                </section>
-              </li>
-              <li className={styles['recruit-content']}>
-                <img
-                  src='/images/新着求人画像3.jpg'
-                  alt='新着求人画像3'
-                  width='370'
-                  height='208'
-                  className={styles['recruit-img']}
-                />
-                <h1 className={styles['recruit-caption']}>
-                  平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
-                </h1>
-                <section>
-                  <div className={styles['recruit-info']}>
-                    <ul>
-                      <li className={styles['info-title']}>給与</li>
-                      <li className={styles['info-title']}>所在地</li>
-                      <li className={styles['info-title']}>最寄駅</li>
-                    </ul>
-                  </div>
-                  <div className={styles['recruit-info']}>
-                    <ul>
-                      <li className={styles['info-detail']}>月給148,000円~</li>
-                      <li className={styles['info-detail']}>三重県伊勢市</li>
-                      <li className={styles['info-detail']}>
-                        伊勢市駅 徒歩15分
-                      </li>
-                    </ul>
-                  </div>
-                </section>
-              </li>
-              <li className={styles['recruit-content']}>
-                <img
-                  src='/images/新着求人画像4.jpg'
-                  alt='新着求人画像4'
-                  width='370'
-                  height='208'
-                  className={styles['recruit-img']}
-                />
-                <h1 className={styles['recruit-caption']}>
-                  平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
-                </h1>
-                <section>
-                  <div className={styles['recruit-info']}>
-                    <ul>
-                      <li className={styles['info-title']}>給与</li>
-                      <li className={styles['info-title']}>所在地</li>
-                      <li className={styles['info-title']}>最寄駅</li>
-                    </ul>
-                  </div>
-                  <div className={styles['recruit-info']}>
-                    <ul>
-                      <li className={styles['info-detail']}>月給148,000円~</li>
-                      <li className={styles['info-detail']}>三重県伊勢市</li>
-                      <li className={styles['info-detail']}>
-                        伊勢市駅 徒歩15分
-                      </li>
-                    </ul>
-                  </div>
-                </section>
-              </li>
-            </ul>
+            <Swiper
+              modules={[Navigation, Pagination]}
+              spaceBetween={20}
+              slidesPerView={1}
+              navigation
+              pagination={{ clickable: true }}
+              loop
+            >
+              {[
+                {
+                  img: '/images/新着求人画像.png',
+                  caption: '伊勢赤十字病院で医療事務求人',
+                },
+                {
+                  img: '/images/新着求人女性2.jpg',
+                  caption: '伊勢赤十字病院で医療事務求人 2',
+                },
+                {
+                  img: '/images/新着求人画像3.jpg',
+                  caption: '伊勢赤十字病院で医療事務求人 3',
+                },
+                {
+                  img: '/images/新着求人画像4.jpg',
+                  caption: '伊勢赤十字病院で医療事務求人 4',
+                },
+              ].map((job, index) => (
+                <SwiperSlide key={index}>
+                  <ul className={styles['recruit-container']}>
+                    <li className={styles['recruit-content']}>
+                      <img
+                        src='/images/新着求人画像.png'
+                        alt='新着求人画像'
+                        width='370'
+                        height='208'
+                        className={styles['recruit-img']}
+                      />
+                      <h1 className={styles['recruit-caption']}>
+                        平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
+                      </h1>
+                      <section>
+                        <div className={styles['recruit-info']}>
+                          <ul>
+                            <li className={styles['info-title']}>給与</li>
+                            <li className={styles['info-title']}>所在地</li>
+                            <li className={styles['info-title']}>最寄駅</li>
+                          </ul>
+                        </div>
+                        <div className={styles['recruit-info']}>
+                          <ul>
+                            <li className={styles['info-detail']}>
+                              月給148,000円~
+                            </li>
+                            <li className={styles['info-detail']}>
+                              三重県伊勢市
+                            </li>
+                            <li className={styles['info-detail']}>
+                              伊勢市駅 徒歩15分
+                            </li>
+                          </ul>
+                        </div>
+                      </section>
+                    </li>
+                    <li className={styles['recruit-content']}>
+                      <img
+                        src='/images/新着求人女性2.jpg'
+                        alt='新着求人画像2'
+                        width='370'
+                        height='208'
+                        className={styles['recruit-img']}
+                      />
+                      <h1 className={styles['recruit-caption']}>
+                        平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
+                      </h1>
+                      <section>
+                        <div className={styles['recruit-info']}>
+                          <ul>
+                            <li className={styles['info-title']}>給与</li>
+                            <li className={styles['info-title']}>所在地</li>
+                            <li className={styles['info-title']}>最寄駅</li>
+                          </ul>
+                        </div>
+                        <div className={styles['recruit-info']}>
+                          <ul>
+                            <li className={styles['info-detail']}>
+                              月給148,000円~
+                            </li>
+                            <li className={styles['info-detail']}>
+                              三重県伊勢市
+                            </li>
+                            <li className={styles['info-detail']}>
+                              伊勢市駅 徒歩15分
+                            </li>
+                          </ul>
+                        </div>
+                      </section>
+                    </li>
+                    <li className={styles['recruit-content']}>
+                      <img
+                        src='/images/新着求人画像3.jpg'
+                        alt='新着求人画像3'
+                        width='370'
+                        height='208'
+                        className={styles['recruit-img']}
+                      />
+                      <h1 className={styles['recruit-caption']}>
+                        平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
+                      </h1>
+                      <section>
+                        <div className={styles['recruit-info']}>
+                          <ul>
+                            <li className={styles['info-title']}>給与</li>
+                            <li className={styles['info-title']}>所在地</li>
+                            <li className={styles['info-title']}>最寄駅</li>
+                          </ul>
+                        </div>
+                        <div className={styles['recruit-info']}>
+                          <ul>
+                            <li className={styles['info-detail']}>
+                              月給148,000円~
+                            </li>
+                            <li className={styles['info-detail']}>
+                              三重県伊勢市
+                            </li>
+                            <li className={styles['info-detail']}>
+                              伊勢市駅 徒歩15分
+                            </li>
+                          </ul>
+                        </div>
+                      </section>
+                    </li>
+                    <li className={styles['recruit-content']}>
+                      <img
+                        src='/images/新着求人画像4.jpg'
+                        alt='新着求人画像4'
+                        width='370'
+                        height='208'
+                        className={styles['recruit-img']}
+                      />
+                      <h1 className={styles['recruit-caption']}>
+                        平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
+                      </h1>
+                      <section>
+                        <div className={styles['recruit-info']}>
+                          <ul>
+                            <li className={styles['info-title']}>給与</li>
+                            <li className={styles['info-title']}>所在地</li>
+                            <li className={styles['info-title']}>最寄駅</li>
+                          </ul>
+                        </div>
+                        <div className={styles['recruit-info']}>
+                          <ul>
+                            <li className={styles['info-detail']}>
+                              月給148,000円~
+                            </li>
+                            <li className={styles['info-detail']}>
+                              三重県伊勢市
+                            </li>
+                            <li className={styles['info-detail']}>
+                              伊勢市駅 徒歩15分
+                            </li>
+                          </ul>
+                        </div>
+                      </section>
+                    </li>
+                  </ul>
+                </SwiperSlide>
+              ))}
+            </Swiper>
             <div className={styles['recruit-button-container']}>
               <button className={styles['recruit-button']}>
                 新着求人一覧をもっと見る
