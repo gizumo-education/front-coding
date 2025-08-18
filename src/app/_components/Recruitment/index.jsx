2b293f458bef355996fc3b0f697aff3ab12ddcff
omochi <omochi@example.com>
2025-08-18 14:31:07 +0900

    ページネーション仕様変更(1枚ずつのスライド)

--- src/app/_components/Recruitment/index.jsx
+++ src/app/_components/Recruitment/index.jsx
@@ -25,165 +25,139 @@ export const Recruitment = () => {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
               }}
-              loop
+              loop={true}
+              breakpoints={{
+                768: { slidesPerView: 4 },
+              }}
             >
               {[
                 {
                   img: '/images/新着求人画像.png',
-                  caption: '伊勢赤十字病院で医療事務求人',
+                  caption:
+                    '平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人',
+                  salaryTitle: '給与',
+                  locationTitle: '所在地',
+                  nearestStationTitle: '最寄り駅',
+                  salary: '月給148,000円~',
+                  location: '三重県伊勢市',
+                  nearestStation: '伊勢市駅 徒歩15分',
                 },
                 {
                   img: '/images/新着求人画像2.jpg',
-                  caption: '伊勢赤十字病院で医療事務求人 2',
+                  caption:
+                    '平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人',
+                  salaryTitle: '給与',
+                  locationTitle: '所在地',
+                  nearestStationTitle: '最寄り駅',
+                  salary: '月給148,000円~',
+                  location: '三重県伊勢市',
+                  nearestStation: '伊勢市駅 徒歩15分',
                 },
                 {
                   img: '/images/新着求人画像3.jpg',
-                  caption: '伊勢赤十字病院で医療事務求人 3',
+                  caption:
+                    '平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人',
+                  salaryTitle: '給与',
+                  locationTitle: '所在地',
+                  nearestStationTitle: '最寄り駅',
+                  salary: '月給148,000円~',
+                  location: '三重県伊勢市',
+                  nearestStation: '伊勢市駅 徒歩15分',
                 },
                 {
                   img: '/images/新着求人画像4.jpg',
-                  caption: '伊勢赤十字病院で医療事務求人 4',
+                  caption:
+                    '平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人',
+                  salaryTitle: '給与',
+                  locationTitle: '所在地',
+                  nearestStationTitle: '最寄り駅',
+                  salary: '月給148,000円~',
+                  location: '三重県伊勢市',
+                  nearestStation: '伊勢市駅 徒歩15分',
+                },
+                {
+                  img: '/images/新着求人画像.png',
+                  caption:
+                    '平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人',
+                  salaryTitle: '給与',
+                  locationTitle: '所在地',
+                  nearestStationTitle: '最寄り駅',
+                  salary: '月給148,000円~',
+                  location: '三重県伊勢市',
+                  nearestStation: '伊勢市駅 徒歩15分',
+                },
+                {
+                  img: '/images/新着求人画像2.jpg',
+                  caption:
+                    '平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人',
+                  salaryTitle: '給与',
+                  locationTitle: '所在地',
+                  nearestStationTitle: '最寄り駅',
+                  salary: '月給148,000円~',
+                  location: '三重県伊勢市',
+                  nearestStation: '伊勢市駅 徒歩15分',
+                },
+                {
+                  img: '/images/新着求人画像3.jpg',
+                  caption:
+                    '平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人',
+                  salaryTitle: '給与',
+                  locationTitle: '所在地',
+                  nearestStationTitle: '最寄り駅',
+                  salary: '月給148,000円~',
+                  location: '三重県伊勢市',
+                  nearestStation: '伊勢市駅 徒歩15分',
+                },
+                {
+                  img: '/images/新着求人画像4.jpg',
+                  caption:
+                    '平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人',
+                  salaryTitle: '給与',
+                  locationTitle: '所在地',
+                  nearestStationTitle: '最寄り駅',
+                  salary: '月給148,000円~',
+                  location: '三重県伊勢市',
+                  nearestStation: '伊勢市駅 徒歩15分',
                 },
               ].map((job, index) => (
-                <SwiperSlide key={index}>
-                  <ul className={styles['recruit-container']}>
-                    <li className={styles['recruit-content']}>
-                      <img
-                        src={job.img}
-                        alt={job.caption}
-                        width='370'
-                        height='208'
-                        className={styles['recruit-img']}
-                      />
-                      <h1 className={styles['recruit-caption']}>
-                        平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
-                      </h1>
-                      <section>
-                        <div className={styles['recruit-info']}>
-                          <ul>
-                            <li className={styles['info-title']}>給与</li>
-                            <li className={styles['info-title']}>所在地</li>
-                            <li className={styles['info-title']}>最寄駅</li>
-                          </ul>
-                        </div>
-                        <div className={styles['recruit-info']}>
-                          <ul>
-                            <li className={styles['info-detail']}>
-                              月給148,000円~
-                            </li>
-                            <li className={styles['info-detail']}>
-                              三重県伊勢市
-                            </li>
-                            <li className={styles['info-detail']}>
-                              伊勢市駅 徒歩15分
-                            </li>
-                          </ul>
-                        </div>
-                      </section>
-                    </li>
-                    <li className={styles['recruit-content']}>
-                      <img
-                        src={job.img}
-                        alt={job.caption}
-                        width='370'
-                        height='208'
-                        className={styles['recruit-img']}
-                      />
-                      <h1 className={styles['recruit-caption']}>
-                        平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
-                      </h1>
-                      <section>
-                        <div className={styles['recruit-info']}>
-                          <ul>
-                            <li className={styles['info-title']}>給与</li>
-                            <li className={styles['info-title']}>所在地</li>
-                            <li className={styles['info-title']}>最寄駅</li>
-                          </ul>
-                        </div>
-                        <div className={styles['recruit-info']}>
-                          <ul>
-                            <li className={styles['info-detail']}>
-                              月給148,000円~
-                            </li>
-                            <li className={styles['info-detail']}>
-                              三重県伊勢市
-                            </li>
-                            <li className={styles['info-detail']}>
-                              伊勢市駅 徒歩15分
-                            </li>
-                          </ul>
-                        </div>
-                      </section>
-                    </li>
-                    <li className={styles['recruit-content']}>
-                      <img
-                        src={job.img}
-                        alt={job.caption}
-                        width='370'
-                        height='208'
-                        className={styles['recruit-img']}
-                      />
-                      <h1 className={styles['recruit-caption']}>
-                        平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
-                      </h1>
-                      <section>
-                        <div className={styles['recruit-info']}>
-                          <ul>
-                            <li className={styles['info-title']}>給与</li>
-                            <li className={styles['info-title']}>所在地</li>
-                            <li className={styles['info-title']}>最寄駅</li>
-                          </ul>
-                        </div>
-                        <div className={styles['recruit-info']}>
-                          <ul>
-                            <li className={styles['info-detail']}>
-                              月給148,000円~
-                            </li>
-                            <li className={styles['info-detail']}>
-                              三重県伊勢市
-                            </li>
-                            <li className={styles['info-detail']}>
-                              伊勢市駅 徒歩15分
-                            </li>
-                          </ul>
-                        </div>
-                      </section>
-                    </li>
-                    <li className={styles['recruit-content']}>
-                      <img
-                        src={job.img}
-                        alt={job.caption}
-                        width='370'
-                        height='208'
-                        className={styles['recruit-img']}
-                      />
-                      <h1 className={styles['recruit-caption']}>
-                        平日・夕方までの勤務！伊勢市の伊勢赤十字病院で医療事務求人
-                      </h1>
-                      <section>
-                        <div className={styles['recruit-info']}>
-                          <ul>
-                            <li className={styles['info-title']}>給与</li>
-                            <li className={styles['info-title']}>所在地</li>
-                            <li className={styles['info-title']}>最寄駅</li>
-                          </ul>
-                        </div>
-                        <div className={styles['recruit-info']}>
-                          <ul>
-                            <li className={styles['info-detail']}>
-                              月給148,000円~
-                            </li>
-                            <li className={styles['info-detail']}>
-                              三重県伊勢市
-                            </li>
-                            <li className={styles['info-detail']}>
-                              伊勢市駅 徒歩15分
-                            </li>
-                          </ul>
-                        </div>
-                      </section>
-                    </li>
-                  </ul>
+                <SwiperSlide
+                  key={index}
+                  className={styles['recruit-container']}
+                >
+                  <li className={styles['recruit-content']}>
+                    <img
+                      src={job.img}
+                      alt={job.caption}
+                      width='370'
+                      height='208'
+                      className={styles['recruit-img']}
+                    />
+                    <h1 className={styles['recruit-caption']}>{job.caption}</h1>
+                    <div className={styles['recruit-info']}>
+                      <ul>
+                        <li className={styles['info-title']}>
+                          {job.salaryTitle}
+                        </li>
+                        <li className={styles['info-title']}>
+                          {job.locationTitle}
+                        </li>
+                        <li className={styles['info-title']}>
+                          {job.nearestStationTitle}
+                        </li>
+                      </ul>
+                    </div>
+                    <div className={styles['recruit-info']}>
+                      <ul>
+                        <li className={styles['info-detail']}>{job.salary}</li>
+                        <li className={styles['info-detail']}>
+                          {job.location}
+                        </li>
+                        <li className={styles['info-detail']}>
+                          {job.nearestStation}
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
                 </SwiperSlide>
               ))}
               <div className={`swiper-button-prev ${styles.prev}`}></div>
