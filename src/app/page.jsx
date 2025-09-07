de2df283431c22f2afb7fdd18e9efbf5c6054586
Hyogo03 <sasahyogo@gmail.com>
2025-09-07 11:45:59 +0900

    add: トップページにコンポーネントを追加

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,17 @@
 import { Header } from '@/components/layout'
+import { Keyvisual } from './_components/Keyvisual'
+import { Jobsearch } from './_components/Jobsearch'
+import { NewJobs } from './_components/NewJobs'
+import { Main } from './_components/Main'
 
 const Top = () => {
   return (
     <>
       <Header />
+      <Keyvisual />
+      <Jobsearch />
+      <NewJobs />
+      <Main />
     </>
   )
 }
