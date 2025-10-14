3ab65db06303be444eb305fb91b9a66d8c24374b
濵田沙羅 <sarada20020628@gmail.com>
2025-10-14 15:22:20 +0900

    add: [PCレスポンシブ]mainvisual作成

--- src/app/_components/feature/index.jsx
+++ src/app/_components/feature/index.jsx
@@ -10,8 +10,10 @@ export const Feature = ({ className = '' }) => {
           <h3 className={styles['feature-box-title']}>WEB上で応募完結</h3>
           <p className={styles['feature-box-description']}>
             エントリーシートの記入を済ませれば、
-            <br />
-            あとは気になる企業に応募して待つだけで面接アポまで完結！
+            <br className={styles['br-sp']} />
+            あとは気になる企業に応募して
+            <br className={styles['br-pc']} />
+            待つだけで面接アポまで完結！
           </p>
         </div>
         <div className={styles['feature-box']}>
@@ -26,9 +28,11 @@ export const Feature = ({ className = '' }) => {
           <h3 className={styles['feature-box-title']}>わかりやすい</h3>
           <p className={styles['feature-box-description']}>
             当求人サイトは医療事務に特化しておりさらに
-            <br />
-            こだわり検索や地域から検索といったご自身に
-            <br />
+            <br className={styles['br-sp']} />
+            こだわり検索や地域から検索
+            <br className={styles['br-pc']} />
+            といったご自身に
+            <br className={styles['br-sp']} />
             ピッタリマッチした求人を見つけることができる！
           </p>
         </div>
@@ -36,10 +40,14 @@ export const Feature = ({ className = '' }) => {
           <h3 className={styles['feature-box-title']}>完全無料</h3>
           <p className={styles['feature-box-description']}>
             当サイトは求人元の企業やグループ会社からのスポンサー費用に
-            <br />
-            よって運営されています。ですので求職者の方からは
-            <br />
-            応募前はもちろん、内定後も費用は一切いただきません。
+            <br className={styles['br-sp']} />
+            よって
+            <br className={styles['br-pc']} />
+            運営されています。ですので求職者の方からは
+            <br className={styles['br-sp']} />
+            応募前はもちろん、
+            <br className={styles['br-pc']} />
+            内定後も費用は一切いただきません。
           </p>
         </div>
       </div>
