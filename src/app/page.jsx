31e7a274f2fcb22725eb9908fb22d04d931377a0
村上　諒 <murakamiryo@murakamiryounoMacBook-Pro.local>
2025-11-06 14:42:45 +0900

    keyvisualの画像像を表示。

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
-import { Header } from '@/components/layout'
+import { Header } from '@/components/layout/Header'
+import { Keyvisual } from '@/components/layout/Keyvisual'
 
 const Top = () => {
   return (
     <>
       <Header />
+      <Keyvisual />
     </>
   )
 }
---
+++ src/components/layout/Keyvisual/index.jsx
@@ -0,0 +1,11 @@
+// import clsx from 'clsx'
+// import styles from './index.module.scss'
+
+export const Keyvisual = () => {
+  return (
+    <div className='p-keyvisual'>
+      <div className='js-carousel slick-initialized slick-slider slick-dotted'></div>
+      <img src='/img/Key-Visual.jpg' alt='' width={10000} height={10000} />
+    </div>
+  )
+}
