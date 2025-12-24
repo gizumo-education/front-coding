aa7f52c156fbd9729a4854a3330011fe69cb6035
新井　路花 <arahana@araijihananoMacBook-Air.local>
2025-12-24 11:13:46 +0900

    pc実装 Backgroundのテキスト位置修正

--- src/app/_components/section/Background/index.jsx
+++ src/app/_components/section/Background/index.jsx
@@ -2,9 +2,9 @@ import styles from './index.module.scss'
 
 export const Background = () => {
   return (
-    <section className={styles['background-section']}>
+    <section className={styles.background}>
       <div className={'inner'}>
-        <p>
+        <p className={styles['background_text']}>
           ギズジョブなら理想の職場がきっと見つかる
           <br />
           ギズジョブは完全無料の求職者向け求人サービスです。
