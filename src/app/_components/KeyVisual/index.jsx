e53c1e1db780534c5de13b69d547e371eac72a9b
yuuna <yuuna@yuunanoMacBook-Pro.local>
2025-12-24 15:26:46 +0900

    push:javascript機能を実装

--- src/app/_components/KeyVisual/index.jsx
+++ src/app/_components/KeyVisual/index.jsx
@@ -3,7 +3,7 @@ import styles from './index.module.scss'
 
 export const KyeVisual = () => {
   return (
-    <section className={clsx(styles.pc_backcolor, styles.sp_backimg)}>
+    <section className={clsx(styles.pc_backcolor, styles.sp_backimg)} id='/'>
       <div className={'inner'}>
         <div className={styles.keyvisual_flex}>
           <div className={styles.section}>
---
+++ src/components/Hamburger/index.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react'
+import clsx from 'clsx'
+import styles from './index.module.scss'
+
+export const Hamburger = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('no-scroll')
+    } else {
+      document.body.classList.remove('no-scroll')
+    }
+    return () => {
+      document.body.classList.remove('no-scroll')
+    }
+  }, [isOpen])
+
+  const handleOpenClick = () => {
+    setIsOpen((prev) => !prev)
+  }
+
+  const handleScroll = (e, id) => {
+    e.preventDefault()
+    const target = document.getElementById(id)
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      })
+    }
+    setIsOpen((prev) => !prev)
+  }
+
+  return (
+    <>
+      <nav className={clsx(styles.hamburger_nav, { [styles.active]: isOpen })}>
+        <ul className={clsx(styles.nav_items)}>
+          <li className={clsx(styles.nav_items_item, styles.nav_items_item1)}>
+            <a href='/' onClick={(e) => handleScroll(e, '/')}>
+              トップ
+            </a>
+          </li>
+          <li className={clsx(styles.nav_items_item, styles.nav_items_item2)}>
+            <a href='#newjobs' onClick={(e) => handleScroll(e, 'newjobs')}>
+              新着求人
+            </a>
+          </li>
+          <li className={clsx(styles.nav_items_item, styles.nav_items_item3)}>
+            <a href='/'>4つの特徴</a>
+          </li>
+          <li className={clsx(styles.nav_items_item, styles.nav_items_item4)}>
+            <a href='/'>転職までの流れ</a>
+          </li>
+          <li className={clsx(styles.nav_items_item, styles.nav_items_item5)}>
+            <a href='/'>転職お役立ちコンテンツ</a>
+          </li>
+        </ul>
+      </nav>
+      <button
+        className={styles.button}
+        onClick={handleOpenClick}
+        aria-expanded={isOpen}
+        aria-controls='hamburger-nav'
+        aria-label='メニューを開閉'
+      >
+        <span className={styles.line} />
+        <span className={styles.line} />
+        <span className={styles.line} />
+      </button>
+    </>
+  )
+}
