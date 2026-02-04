b2eb06da2f7929b604be4de47578bb700cdfb13f
海老名彩希 <ebinamasato@MacBook-Air-3.local>
2026-02-04 11:08:53 +0900

    ハンバーガーメニュー修正

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,43 @@
+'use client'
+
 import { Header } from '@/components/layout'
 import { Hero } from './_components/Hero'
 import { JobSearch } from './_components/JobSearch'
 import { JobOpening } from './_components/JobOpening'
 import { SubHeading } from './_components/SubHeading'
+import { useRef, useState, useEffect } from 'react'
 
 const Top = () => {
+  const appRootRef = useRef(null)
+  const scrollYRef = useRef(0)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  useEffect(() => {
+    const root = appRootRef.current
+
+    if (!root) return
+
+    if (isMenuOpen) {
+      scrollYRef.current = window.scrollY
+      root.style.top = `-${scrollYRef.current}px`
+      root.classList.add('scroll-lock')
+    } else {
+      root.classList.remove('scroll-lock')
+      root.style.top = ''
+      window.scrollTo(0, scrollYRef.current)
+    }
+  }, [isMenuOpen])
+
   return (
-    <>
-      <Header />
+    <div ref={appRootRef}>
+      <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <main>
         <Hero />
         <JobSearch />
         <JobOpening />
       </main>
       <SubHeading />
-    </>
+    </div>
   )
 }
 
--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -2,8 +2,22 @@
 
 import clsx from 'clsx'
 import styles from './index.module.scss'
+import { useRef } from 'react'
 
-export const Header = () => {
+export const Header = ({ isMenuOpen, setIsMenuOpen }) => {
+  const menuRef = useRef(null)
+  const buttonRef = useRef(null)
+
+  const toggleMenu = () => {
+    menuRef.current.classList.toggle(styles['menu-open'])
+    buttonRef.current.classList.toggle(styles['button-isopen'])
+    setIsMenuOpen((prev) => !prev)
+  }
+
+  const closeMenu = () => {
+    menuRef.current.classList.remove(styles['menu-open'])
+    buttonRef.current.classList.remove(styles['button-isopen'])
+  }
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
@@ -42,11 +56,45 @@ export const Header = () => {
           </li>
         </ul>
         <div className='menu'>
-          <button className={styles.button}>
-            <span className={styles.line} />
-            <span className={styles.line} />
-            <span className={styles.line} />
+          <button
+            type='button'
+            ref={buttonRef}
+            className={clsx(styles.button, isMenuOpen && styles.open)}
+            onClick={toggleMenu}
+          >
+            <span className={clsx(styles['button-line-top'], styles.line)} />
+            <span className={clsx(styles['button-line-medium'], styles.line)} />
+            <span className={clsx(styles['button-line-bottom'], styles.line)} />
           </button>
+        </div>
+        <div ref={menuRef} className={styles['menu-list']}>
+          <ul>
+            <li className={styles['list-item']}>
+              <a href='#top' onClick={closeMenu}>
+                トップ
+              </a>
+            </li>
+            <li className={styles['list-item']}>
+              <a href='/#job-opening' onClick={closeMenu}>
+                新着求人
+              </a>
+            </li>
+            <li className={styles['list-item']}>
+              <a href='/' onClick={closeMenu}>
+                ４つの特徴
+              </a>
+            </li>
+            <li className={styles['list-item']}>
+              <a href='/' onClick={closeMenu}>
+                転職までの流れ
+              </a>
+            </li>
+            <li className={styles['list-item']}>
+              <a href='/' onClick={closeMenu}>
+                転職お役立ちコンテンツ
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
