34254e5cb071a63e33b902e1a194575fc9b5ea9b
shinmoto <shinmoto@gizumo-admin-44noMacBook-Air.local>
2026-02-13 14:59:29 +0900

    前半セクション：PC画面実装

--- src/app/_components/JobOffer/index.jsx
+++ src/app/_components/JobOffer/index.jsx
@@ -6,19 +6,17 @@ export const JobOffer = () => {
       <section className={styles.section}>
         <div className={styles.inner}>
           <h2 className={styles.title}>新着求人</h2>
-          <ul className={styles.list}>
+          <ul className={`${styles.list} ${styles.carousel}`}>
             <li className={styles['list-item']}>
-              <div className={styles.carousel}>
-                <img
-                  src='/img/job_01.jpg'
-                  alt='求人情報01'
-                  className={styles['job-img']}
-                  width={370}
-                  height={208}
-                />
-                <button className={`${styles.arrow} ${styles.left}`}></button>
-                <button className={`${styles.arrow} ${styles.right}`}></button>
-              </div>
+              <img
+                src='/img/job_01.jpg'
+                alt='求人情報01'
+                className={styles['job-img']}
+                width={370}
+                height={208}
+              />
+              <button className={`${styles.arrow} ${styles.left}`}></button>
+              <button className={`${styles.arrow} ${styles.right}`}></button>
               <h3 className={styles['job-title']}>
                 平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人
               </h3>
@@ -36,11 +34,95 @@ export const JobOffer = () => {
                   <p className={styles.content}>伊勢市駅 徒歩15分</p>
                 </div>
               </div>
-              <button className={styles['is-search']}>
-                新着求人一覧をもっと見る
-              </button>
+            </li>
+            <li className={styles['list-item']}>
+              <img
+                src='/img/job_02.jpg'
+                alt='求人情報02'
+                className={styles['job-img']}
+                width={370}
+                height={208}
+              />
+              <button className={`${styles.arrow} ${styles.left}`}></button>
+              <button className={`${styles.arrow} ${styles.right}`}></button>
+              <h3 className={styles['job-title']}>
+                平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人
+              </h3>
+              <div className={styles['text-container']}>
+                <div className={styles['text-box']}>
+                  <p className={styles.category}>給与</p>
+                  <p className={styles.content}>月給 148,000円～</p>
+                </div>
+                <div className={styles['text-box']}>
+                  <p className={styles.category}>所在地</p>
+                  <p className={styles.content}>三重県伊勢市</p>
+                </div>
+                <div className={styles['text-box']}>
+                  <p className={styles.category}>最寄駅</p>
+                  <p className={styles.content}>伊勢市駅 徒歩15分</p>
+                </div>
+              </div>
+            </li>
+            <li className={styles['list-item']}>
+              <img
+                src='/img/job_03.jpg'
+                alt='求人情報03'
+                className={styles['job-img']}
+                width={370}
+                height={208}
+              />
+              <button className={`${styles.arrow} ${styles.left}`}></button>
+              <button className={`${styles.arrow} ${styles.right}`}></button>
+              <h3 className={styles['job-title']}>
+                平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人
+              </h3>
+              <div className={styles['text-container']}>
+                <div className={styles['text-box']}>
+                  <p className={styles.category}>給与</p>
+                  <p className={styles.content}>月給 148,000円～</p>
+                </div>
+                <div className={styles['text-box']}>
+                  <p className={styles.category}>所在地</p>
+                  <p className={styles.content}>三重県伊勢市</p>
+                </div>
+                <div className={styles['text-box']}>
+                  <p className={styles.category}>最寄駅</p>
+                  <p className={styles.content}>伊勢市駅 徒歩15分</p>
+                </div>
+              </div>
+            </li>
+            <li className={styles['list-item']}>
+              <img
+                src='/img/job_04.jpg'
+                alt='求人情報04'
+                className={styles['job-img']}
+                width={370}
+                height={208}
+              />
+              <button className={`${styles.arrow} ${styles.left}`}></button>
+              <button className={`${styles.arrow} ${styles.right}`}></button>
+              <h3 className={styles['job-title']}>
+                平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人
+              </h3>
+              <div className={styles['text-container']}>
+                <div className={styles['text-box']}>
+                  <p className={styles.category}>給与</p>
+                  <p className={styles.content}>月給 148,000円～</p>
+                </div>
+                <div className={styles['text-box']}>
+                  <p className={styles.category}>所在地</p>
+                  <p className={styles.content}>三重県伊勢市</p>
+                </div>
+                <div className={styles['text-box']}>
+                  <p className={styles.category}>最寄駅</p>
+                  <p className={styles.content}>伊勢市駅 徒歩15分</p>
+                </div>
+              </div>
             </li>
           </ul>
+          <button className={styles['is-search']}>
+            新着求人一覧をもっと見る
+          </button>
         </div>
       </section>
     </>
