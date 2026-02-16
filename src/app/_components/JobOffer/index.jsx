f505826f9998e1248fdccffd11a1ab96a30a913c
shinmoto <shinmoto@gizumo-admin-44noMacBook-Air.local>
2026-02-16 17:53:14 +0900

    JS実装

--- src/app/_components/JobOffer/index.jsx
+++ src/app/_components/JobOffer/index.jsx
@@ -1,130 +1,126 @@
+'use client'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
 import styles from './index.module.scss'
 
 export const JobOffer = () => {
+  const jobs = [
+    {
+      id: 1,
+      title: '平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人',
+      img: '/img/job_01.jpg',
+      alt: '求人情報01',
+      money: '月給 148,000円～',
+      place: '三重県伊勢市',
+      station: '伊勢市駅 徒歩15分',
+    },
+    {
+      id: 2,
+      title: '平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人',
+      img: '/img/job_02.jpg',
+      alt: '求人情報02',
+      money: '月給 148,000円～',
+      place: '三重県伊勢市',
+      station: '伊勢市駅 徒歩15分',
+    },
+    {
+      id: 3,
+      title: '平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人',
+      img: '/img/job_03.jpg',
+      alt: '求人情報03',
+      money: '月給 148,000円～',
+      place: '三重県伊勢市',
+      station: '伊勢市駅 徒歩15分',
+    },
+    {
+      id: 4,
+      title: '平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人',
+      img: '/img/job_04.jpg',
+      alt: '求人情報04',
+      money: '月給 148,000円～',
+      place: '三重県伊勢市',
+      station: '伊勢市駅 徒歩15分',
+    },
+    {
+      id: 5,
+      title: '平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人',
+      img: '/img/job_01.jpg',
+      alt: '求人情報05',
+      money: '月給 148,000円～',
+      place: '三重県伊勢市',
+      station: '伊勢市駅 徒歩15分',
+    },
+  ]
+
   return (
-    <>
-      <section className={styles.section}>
-        <div className={styles.inner}>
-          <h2 className={styles.title}>新着求人</h2>
-          <ul className={`${styles.list} ${styles.carousel}`}>
-            <li className={styles['list-item']}>
-              <img
-                src='/img/job_01.jpg'
-                alt='求人情報01'
-                className={styles['job-img']}
-                width={370}
-                height={208}
-              />
-              <button className={`${styles.arrow} ${styles.left}`}></button>
-              <button className={`${styles.arrow} ${styles.right}`}></button>
-              <h3 className={styles['job-title']}>
-                平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人
-              </h3>
-              <div className={styles['text-container']}>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>給与</p>
-                  <p className={styles.content}>月給 148,000円～</p>
-                </div>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>所在地</p>
-                  <p className={styles.content}>三重県伊勢市</p>
-                </div>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>最寄駅</p>
-                  <p className={styles.content}>伊勢市駅 徒歩15分</p>
-                </div>
-              </div>
-            </li>
-            <li className={styles['list-item']}>
-              <img
-                src='/img/job_02.jpg'
-                alt='求人情報02'
-                className={styles['job-img']}
-                width={370}
-                height={208}
-              />
-              <button className={`${styles.arrow} ${styles.left}`}></button>
-              <button className={`${styles.arrow} ${styles.right}`}></button>
-              <h3 className={styles['job-title']}>
-                平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人
-              </h3>
-              <div className={styles['text-container']}>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>給与</p>
-                  <p className={styles.content}>月給 148,000円～</p>
-                </div>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>所在地</p>
-                  <p className={styles.content}>三重県伊勢市</p>
-                </div>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>最寄駅</p>
-                  <p className={styles.content}>伊勢市駅 徒歩15分</p>
-                </div>
-              </div>
-            </li>
-            <li className={styles['list-item']}>
-              <img
-                src='/img/job_03.jpg'
-                alt='求人情報03'
-                className={styles['job-img']}
-                width={370}
-                height={208}
-              />
-              <button className={`${styles.arrow} ${styles.left}`}></button>
-              <button className={`${styles.arrow} ${styles.right}`}></button>
-              <h3 className={styles['job-title']}>
-                平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人
-              </h3>
-              <div className={styles['text-container']}>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>給与</p>
-                  <p className={styles.content}>月給 148,000円～</p>
-                </div>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>所在地</p>
-                  <p className={styles.content}>三重県伊勢市</p>
-                </div>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>最寄駅</p>
-                  <p className={styles.content}>伊勢市駅 徒歩15分</p>
-                </div>
-              </div>
-            </li>
-            <li className={styles['list-item']}>
-              <img
-                src='/img/job_04.jpg'
-                alt='求人情報04'
-                className={styles['job-img']}
-                width={370}
-                height={208}
-              />
-              <button className={`${styles.arrow} ${styles.left}`}></button>
-              <button className={`${styles.arrow} ${styles.right}`}></button>
-              <h3 className={styles['job-title']}>
-                平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人
-              </h3>
-              <div className={styles['text-container']}>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>給与</p>
-                  <p className={styles.content}>月給 148,000円～</p>
-                </div>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>所在地</p>
-                  <p className={styles.content}>三重県伊勢市</p>
-                </div>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>最寄駅</p>
-                  <p className={styles.content}>伊勢市駅 徒歩15分</p>
-                </div>
-              </div>
-            </li>
-          </ul>
-          <button className={styles['is-search']}>
-            新着求人一覧をもっと見る
+    <section className={styles.section} id='JobOffer'>
+      <div className={'inner'}>
+        <div className={styles.job}>
+          <h2 className={styles['job-title']}>新着求人</h2>
+          <div className={styles['job-box']}>
+            <Swiper
+              modules={[Navigation]}
+              spaceBetween={12}
+              slidesPerView={1}
+              loop={true}
+              navigation={{
+                prevEl: `.${styles.prev}`,
+                nextEl: `.${styles.next}`,
+              }}
+              breakpoints={{
+                768: {
+                  slidesPerView: 4,
+                  spaceBetween: 20,
+                },
+              }}
+              pagination={false}
+            >
+              {jobs.map((job) => (
+                <SwiperSlide key={job.id}>
+                  <article className={styles.card}>
+                    <img
+                      src={job.img}
+                      alt={job.alt}
+                      className={styles.image}
+                      width={740}
+                      height={416}
+                    />
+                    <h3 className={styles.title}>{job.title}</h3>
+                    <div className={styles.content}>
+                      <p className={styles.item}>
+                        <span className={styles.text}>給与</span>
+                        {job.money}
+                      </p>
+                      <p className={styles.item}>
+                        <span className={styles.text}>所在地</span>
+                        {job.place}
+                      </p>
+                      <p className={styles.item}>
+                        <span className={styles.text}>最寄駅</span>
+                        {job.station}
+                      </p>
+                    </div>
+                  </article>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <button className={styles.prev}>
+            <i className={styles['prev-img']}></i>
           </button>
+          <button className={styles.next}>
+            <i className={styles['next-img']}></i>
+          </button>
+          <a href='/' className={styles.button}>
+            新着求人一覧をもっと見る
+          </a>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   )
 }
