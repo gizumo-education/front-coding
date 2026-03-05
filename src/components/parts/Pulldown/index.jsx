033925f27c9505148692704d31365888e704eff5
taikifujino0511 <taiki.fujino@giztech-pro.com>
2026-03-05 11:29:18 +0900

    Add: プルダウンコンポーネントの追加

---
+++ src/components/parts/Pulldown/index.jsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx'
+import styles from './index.module.scss'
+
+export const Pulldown = ({ name, id, data = [] }) => {
+  return (
+    <div className={clsx(styles['pulldown-wrap'])}>
+      <select name={name} id={id} className={clsx(styles['pulldown'])}>
+        {data?.map((items) => (
+          <option key={items.id} value={items.value}>
+            {items.name}
+          </option>
+        ))}
+      </select>
+      <span className={clsx(styles['pulldown-triangle'])}></span>
+    </div>
+  )
+}
