5d133f61e52073ee5df2044db450fd1cdc69faea
shinmoto <shinmoto@gizumo-admin-44noMacBook-Air.local>
2026-04-08 16:27:56 +0900

    refactor: 不要なフラグメントを削除

--- src/app/_components/JobOffer/index.jsx
+++ src/app/_components/JobOffer/index.jsx
@@ -2,47 +2,45 @@ import styles from './index.module.scss'
 
 export const JobOffer = () => {
   return (
-    <>
-      <section className={styles.section}>
-        <div className={styles.inner}>
-          <h2 className={styles.title}>新着求人</h2>
-          <ul className={styles.list}>
-            <li className={styles['list-item']}>
-              <div className={styles.carousel}>
-                <img
-                  src='/img/job_01.jpg'
-                  alt='求人情報01'
-                  className={styles['job-img']}
-                  width={370}
-                  height={208}
-                />
-                <button className={`${styles.arrow} ${styles.left}`}></button>
-                <button className={`${styles.arrow} ${styles.right}`}></button>
+    <section className={styles.section}>
+      <div className={styles.inner}>
+        <h2 className={styles.title}>新着求人</h2>
+        <ul className={styles.list}>
+          <li className={styles['list-item']}>
+            <div className={styles.carousel}>
+              <img
+                src='/img/job_01.jpg'
+                alt='求人情報01'
+                className={styles['job-img']}
+                width={370}
+                height={208}
+              />
+              <button className={`${styles.arrow} ${styles.left}`}></button>
+              <button className={`${styles.arrow} ${styles.right}`}></button>
+            </div>
+            <h3 className={styles['job-title']}>
+              平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人
+            </h3>
+            <div className={styles['text-container']}>
+              <div className={styles['text-box']}>
+                <p className={styles.category}>給与</p>
+                <p className={styles.content}>月給 148,000円～</p>
               </div>
-              <h3 className={styles['job-title']}>
-                平日・夕方までの勤務!伊勢市の伊勢赤十字病院で医療事務求人
-              </h3>
-              <div className={styles['text-container']}>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>給与</p>
-                  <p className={styles.content}>月給 148,000円～</p>
-                </div>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>所在地</p>
-                  <p className={styles.content}>三重県伊勢市</p>
-                </div>
-                <div className={styles['text-box']}>
-                  <p className={styles.category}>最寄駅</p>
-                  <p className={styles.content}>伊勢市駅 徒歩15分</p>
-                </div>
+              <div className={styles['text-box']}>
+                <p className={styles.category}>所在地</p>
+                <p className={styles.content}>三重県伊勢市</p>
               </div>
-              <button className={styles['is-search']}>
-                新着求人一覧をもっと見る
-              </button>
-            </li>
-          </ul>
-        </div>
-      </section>
-    </>
+              <div className={styles['text-box']}>
+                <p className={styles.category}>最寄駅</p>
+                <p className={styles.content}>伊勢市駅 徒歩15分</p>
+              </div>
+            </div>
+            <button className={styles['is-search']}>
+              新着求人一覧をもっと見る
+            </button>
+          </li>
+        </ul>
+      </div>
+    </section>
   )
 }
--- src/app/_components/KeyVisual/index.jsx
+++ src/app/_components/KeyVisual/index.jsx
@@ -2,29 +2,27 @@ import styles from './index.module.scss'
 
 export const KeyVisual = () => {
   return (
-    <>
-      <section className={styles['section']}>
-        <img
-          src='/img/keyVisual.jpg'
-          alt='キービジュアル'
-          width={400}
-          height={270}
-          className={styles['key-visual']}
-        />
-        <img
-          src='/img/no-1.png'
-          alt='no.1'
-          width={150}
-          height={73}
-          className={styles.no1}
-        />
-        <h1 className={styles.title}>医療事務求人を探すならギズジョブ</h1>
-        <div className={styles['text-box']}>
-          <p className={styles.text}>
-            現在の求人掲載数：<span className={styles['is-big']}>2,320</span>件
-          </p>
-        </div>
-      </section>
-    </>
+    <section className={styles['section']}>
+      <img
+        src='/img/keyVisual.jpg'
+        alt='キービジュアル'
+        width={400}
+        height={270}
+        className={styles['key-visual']}
+      />
+      <img
+        src='/img/no-1.png'
+        alt='no.1'
+        width={150}
+        height={73}
+        className={styles.no1}
+      />
+      <h1 className={styles.title}>医療事務求人を探すならギズジョブ</h1>
+      <div className={styles['text-box']}>
+        <p className={styles.text}>
+          現在の求人掲載数：<span className={styles['is-big']}>2,320</span>件
+        </p>
+      </div>
+    </section>
   )
 }
