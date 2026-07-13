efb362b15adcd160190afaf16cb5dfaba9bbd792
3tsuya <mi3ider@gmail.com>
2026-07-13 20:32:35 +0900

    虫眼鏡をiconに移動

--- src/app/_components/SearchForm/index.jsx
+++ src/app/_components/SearchForm/index.jsx
@@ -89,7 +89,7 @@ export const SearchForm = () => {
         <button className={styles['search-button']} type='submit'>
           <img
             className={styles['search-img']}
-            src='/images/search.svg'
+            src='/icon/search.svg'
             alt='検索ボタン'
             width={18}
             height={19}
