3a5dd47580d105e6f297b4cdea40c826fb6c9e97
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-09-16 13:03:03 +0900

    style: PCのスタイルを調整

--- src/app/_components/Banner/index.jsx
+++ src/app/_components/Banner/index.jsx
@@ -4,10 +4,10 @@ export const Banner = () => {
   return (
     <section className={styles.banner}>
       <p className={styles.text}>
-        <span className={styles['text-bold']}>
+        <span className={styles['text-bold-1']}>
           ギズジョブなら理想の職場がきっと見つかる
         </span>
-        <span className={styles['text-bold']}>
+        <span className={styles['text-bold-2']}>
           ギズジョブは完全無料の求職者向け求人サービスです
           <span className={styles['text-normal']}>。</span>
         </span>
